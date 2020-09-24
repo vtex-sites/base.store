eaeaae4d1b470ba7e8bee72884d24721c0b30294
Emerson Laurentino <emersonlaurentino@hotmail.com>
2020-09-24 12:51:37 -0300

    chore: turning off bypass at the lighthouse

--- lighthouserc.js
+++ lighthouserc.js
@@ -12,87 +12,28 @@ module.exports = {
   ci: {
     collect: {
       url: urls,
-      numberOfRuns: 3,
+      numberOfRuns: 5,
     },
     assert: {
       preset: 'lighthouse:no-pwa',
       assertions: {
-        'uses-long-cache-ttl': 'off',
-        'is-crawlable': 'off', // preview pages are not crawlable
-        'uses-rel-preconnect': 'warn', // somehow lighthouse ci can't find the dns-prefetch tags we are adding
+        'categories:accessibility': ['error', { minScore: 0.95 }],
+        'categories:best-practices': ['error', { minScore: 0.95 }],
+        'categories:performance': ['error', { minScore: 0.95 }],
         'categories:pwa': 'off',
-        'uses-text-compression': [
-          'warn',
-          {
-            maxLength: 1, // rc.js
-          },
-        ],
-        'categories:seo': [
-          'error',
-          {
-            minScore: 0.9,
-          },
-        ],
-        'categories:accessibility': [
-          'error',
-          {
-            minScore: 0.95,
-          },
-        ],
-        'categories:best-practices': [
-          'error',
-          {
-            minScore: 0.95,
-          },
-        ],
-        'categories:performance': [
-          'error',
-          {
-            minScore: 0.95,
-          },
-        ],
-        'unused-javascript': [
-          'error',
-          {
-            maxLength: 10,
-          },
-        ],
-        'largest-contentful-paint': [
-          'error',
-          {
-            maxNumericValue: 3500,
-          },
-        ],
-        'first-contentful-paint': [
-          'error',
-          {
-            maxNumericValue: 2000,
-          },
-        ],
-        interactive: [
-          'error',
-          {
-            maxNumericValue: 3000,
-          },
-        ],
-        'max-potential-fid': [
-          'error',
-          {
-            maxNumericValue: 300,
-          },
-        ],
-        'cumulative-layout-shift': [
-          'error',
-          {
-            maxNumericValue: 0.2,
-          },
-        ],
-        'total-blocking-time': [
-          'error',
-          {
-            maxNumericValue: 250,
-          },
-        ],
+        'categories:seo': ['error', { minScore: 0.9 }],
+        'cumulative-layout-shift': ['error', { maxNumericValue: 0.2 }],
+        'first-contentful-paint': ['error', { maxNumericValue: 2000 }],
+        'is-crawlable': 'off', // preview pages are not crawlable
+        'largest-contentful-paint': ['error', { maxNumericValue: 3500 }],
+        'max-potential-fid': ['error', { maxNumericValue: 300 }],
+        'total-blocking-time': ['error', { maxNumericValue: 200 }],
+        'unused-javascript': ['error', { maxLength: 10 }],
+        'uses-long-cache-ttl': 'off',
+        'uses-rel-preconnect': 'warn', // somehow lighthouse ci can't find the dns-prefetch tags we are adding
+        'uses-text-compression': ['warn', { maxLength: 1 }],
+        bypass: 'off',
+        interactive: ['error', { maxNumericValue: 3000 }],
       },
     },
     upload: {
