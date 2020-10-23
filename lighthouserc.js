01c3b4565875423ad3768b4a4ad1bdff024bfa70
DHasuda <dhasuda@users.noreply.github.com>
2020-10-23 10:44:22 -0300

    Removing lhci github action (#302)
    * Removing lhci github action
    
    * changeg lighthouse urls
    
    Co-authored-by: Tiago Gimenes <tlgimenes@gmail.com>

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,11 +1,5 @@
 const VTEXLHConfig = require('@vtex/lighthouse-config').default
 
-const { BASE_SITE_URL } = process.env
+const urls = ['', '/vintage-phone/p', '/apparel---accessories']
 
-const server = BASE_SITE_URL
-  ? BASE_SITE_URL.replace('.com', '.app')
-  : 'http://localhost:9000'
-
-const urls = ['/', '/vintage-phone/p/', '/apparel---accessories/']
-
-module.exports = VTEXLHConfig({ urls, server })
+module.exports = VTEXLHConfig({ urls, server: process.env.BASE_SITE_URL })
