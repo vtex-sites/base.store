c302b4a0ae2d108b1063445e7b81c8b4253a8289
Igor Brasileiro <brasileiro456@gmail.com>
2021-04-26 14:01:10 -0300

    Revert "Disable ViewCart Button when orderform is loading (#759)" (#773)
    This reverts commit fa72d9f56ccdd8d4fecdec47875c217f3360ef9c.

--- src/@vtex/gatsby-plugin-theme-ui/minicart.ts
+++ src/@vtex/gatsby-plugin-theme-ui/minicart.ts
@@ -19,17 +19,8 @@ const custom: SxStyleProp = {
       footer: {
         button: {
           cursor: 'pointer',
-          '&:not(:disabled):hover': {
+          '&:hover': {
             backgroundColor: '#072c75',
-          },
-          '&:disabled': {
-            background: '#eeeeee',
-          },
-        },
-        checkout: {
-          spinner: {
-            display: 'flex',
-            alignItems: 'center',
           },
         },
       },
