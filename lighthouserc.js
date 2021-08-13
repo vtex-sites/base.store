e9e33f5a00cad02cc1c644e336c64c07ddbb5ae6
Tiago Gimenes <tlgimenes@gmail.com>
2021-08-13 19:23:34 -0300

    fix lighthouse path

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,5 @@
 const VTEXLHConfig = require('@vtex/lighthouse-config').default
 
-const urls = ['', '/women', '/organza-sleeve-top/p']
+const urls = ['', '/women/', '/organza-sleeve-top/p']
 
 module.exports = VTEXLHConfig({ urls, server: process.env.BASE_SITE_URL })
