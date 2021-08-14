635a5531cef82ce8e3ca1eeec804832cf7476d5b
Tiago Gimenes <tlgimenes@gmail.com>
2021-08-13 23:05:51 -0300

    fix lighthouse assertion

--- lighthouserc.js
+++ lighthouserc.js
@@ -2,4 +2,10 @@ const VTEXLHConfig = require('@vtex/lighthouse-config').default
 
 const urls = ['', '/women/', '/organza-sleeve-top/p']
 
-module.exports = VTEXLHConfig({ urls, server: process.env.BASE_SITE_URL })
+module.exports = VTEXLHConfig({
+  urls,
+  server: process.env.BASE_SITE_URL,
+  assertions: {
+    'legacy-javascript': ['error', { maxNumericalValue: 1 }],
+  },
+})
