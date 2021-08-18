031dc0a763043bb811b84b667e863e20b9925f96
Tiago Gimenes <tlgimenes@gmail.com>
2021-08-18 13:22:51 -0300

    fix ssr

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,8 +2,12 @@
 import React from 'react'
 import { SessionProvider, UIProvider } from '@vtex/store-sdk'
 
+import ErrorBoundary from './src/components/error/ErrorBoundary'
+
 export const wrapRootElement = ({ element }) => (
-  <UIProvider>
-    <SessionProvider>{element}</SessionProvider>
-  </UIProvider>
+  <ErrorBoundary>
+    <UIProvider>
+      <SessionProvider>{element}</SessionProvider>
+    </UIProvider>
+  </ErrorBoundary>
 )
