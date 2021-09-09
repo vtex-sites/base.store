e9a337815e01bc12688e0b00136dbff10416cf0d
Tiago Gimenes <tlgimenes@gmail.com>
2021-09-09 07:22:48 -0300

    rollbacck to fashion europe

--- gatsby-config.js
+++ gatsby-config.js
@@ -152,8 +152,6 @@ module.exports = {
       options: {
         sourceProducts: true,
         sourceCollections: true,
-        maxNumProducts: 100,
-        maxNumCollections: 2000,
         getSchema,
       },
     },
