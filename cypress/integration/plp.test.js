c848ec21321b112ab25172ff77f8cd5cd245f5de
Tiago Gimenes <tlgimenes@gmail.com>
2021-09-24 15:06:07 -0300

    feat: Sticky search pagination (#37)

--- cypress/integration/plp.test.js
+++ cypress/integration/plp.test.js
@@ -36,13 +36,13 @@ describe('Search page Filters and Sorting options', () => {
       })
   })
 
-  it('Sort products by price-asc', () => {
+  it('Sort products by price_asc', () => {
     cy.visit(pages.collection, options)
     cy.waitForHydration()
 
     cy.getById('search-sort')
       .should('exist')
-      .select('price-asc')
+      .select('price_asc')
       .then(() => {
         cy.getById('price').should(($prices) => {
           const prices = Cypress._.map($prices, (price) =>
@@ -57,10 +57,10 @@ describe('Search page Filters and Sorting options', () => {
       })
   })
 
-  it('Sort products by price-desc', () => {
+  it('Sort products by price_desc', () => {
     cy.getById('search-sort')
       .should('exist')
-      .select('price-desc')
+      .select('price_desc')
       .then(() => {
         cy.getById('price').should(($prices) => {
           const prices = Cypress._.map($prices, (price) =>
@@ -72,6 +72,111 @@ describe('Search page Filters and Sorting options', () => {
           expect(prices).to.have.length.gt(0)
           expect(prices).to.deep.equal(sorted)
         })
+      })
+  })
+})
+
+describe('Infinite Scroll pagination', () => {
+  beforeEach(() => {
+    cy.clearIDB()
+  })
+
+  it('Shows more products when requested', () => {
+    cy.visit(pages.collection, options)
+    cy.waitForHydration()
+
+    cy.getById('product-link')
+      .should('exist')
+      .should('have.length.gt', 0)
+      .then(($links) => {
+        const before = $links.length
+
+        cy.getById('show-more')
+          .should('exist')
+          .click()
+          .then(() => {
+            cy.getById('product-link')
+              .should('have.length.gte', before)
+              .then(($products) => {
+                const after = $products.length
+
+                expect(before).to.be.lte(after)
+              })
+          })
+      })
+  })
+
+  it('Sticks to last seen page on plp pagination', () => {
+    cy.visit(pages.collection, options)
+    cy.waitForHydration()
+
+    cy.getById('product-link')
+      .should('exist')
+      .should('have.length.gt', 0)
+      .then(($links) => {
+        // Number of products before showMore is clicked
+        const before = $links.length
+
+        cy.getById('show-more')
+          .should('exist')
+          .click()
+          .then(() => {
+            // The skuId of the last product on the page
+            let skuIdBeforeNavigate
+
+            cy.getById('product-link')
+              // Number of products after showMore is clicked should be higher
+              .should('have.length.gte', before)
+              .last()
+              .within(() => {
+                cy.getById('buy-button').then(($btn) => {
+                  skuIdBeforeNavigate = $btn.attr('data-sku')
+                })
+              })
+              .click()
+              .then(() => {
+                // make sure we are on the pdp
+                cy.location('pathname').should('match', /\/p$/)
+              })
+              .then(() => {
+                cy.go('back')
+                  .getById('buy-button')
+                  .last()
+                  .then(($btn) => {
+                    const skuIdAfterNavigate = $btn.attr('data-sku')
+
+                    expect(skuIdBeforeNavigate).to.eq(skuIdAfterNavigate)
+                  })
+              })
+          })
+      })
+  })
+
+  // Tests: https://developers.google.com/search/blog/2014/02/infinite-scroll-search-friendly
+  it('Changes the page being viewed on scroll', () => {
+    cy.visit(pages.collection, options)
+    cy.waitForHydration()
+
+    cy.getById('show-more')
+      .should('exist')
+      .click()
+      .then(() => {
+        cy.getById('product-link')
+          .last()
+          .scrollIntoView()
+          .location()
+          .should(($loc) => {
+            expect($loc.search).includes('page')
+            expect($loc.pathname).includes('/1')
+          })
+          .getById('product-link')
+          .first()
+          .scrollIntoView()
+          .location()
+          .should(($loc) => {
+            expect($loc.search).includes('page')
+            expect($loc.pathname).includes('/0')
+          })
       })
   })
 })
