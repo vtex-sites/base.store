4426304bf8f8fad11ad3f177673a97d3c4031990
Heitor Carvalho <heitor.carvalho@vtex.com.br>
2021-09-27 15:01:00 -0300

    feat: create AnalyticsProvider component

---
+++ src/sdk/analytics/index.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from 'react'
+import { useAnalyticsEvent } from '@vtex/store-sdk'
+import type { FC } from 'react'
+
+export const AnalyticsProvider: FC = ({ children }) => {
+  useEffect(() => {
+    window.dataLayer = window.dataLayer || []
+  })
+
+  useAnalyticsEvent((event) => {
+    window.dataLayer.push(event)
+  })
+
+  return <> {children} </>
+}
+
+export default AnalyticsProvider
