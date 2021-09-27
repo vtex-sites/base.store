ca7183197c9fecbec69e078531f472d68d14215f
Tiago Gimenes <tlgimenes@gmail.com>
2021-09-27 16:03:25 -0300

    loading prismic preview

--- src/pages/preview.tsx
+++ src/pages/preview.tsx
@@ -1,13 +1,9 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { linkResolver } from 'src/utils/linkResolver'
 import { withPrismicPreviewResolver } from 'gatsby-plugin-prismic-previews'
 
 function Page() {
-  useEffect(() => {
-    window.location.href = '/'
-  }, [])
-
-  return <div>loading...</div>
+  return <div>Loading Prismic Preview...</div>
 }
 
 export default withPrismicPreviewResolver(Page, [
