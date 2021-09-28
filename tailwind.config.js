e686363531cbba0049a905cde10ae0a273b4379a
Emerson Laurentino <emerson@laurentino.co>
2021-09-28 13:06:02 -0300

    fix: tailwind purge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   mode: 'jit',
-  purge: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
