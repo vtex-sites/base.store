1e4ed0e271e06c0d537509b7bc0e698e96933ada
Igor Brasileiro <brasileiro456@gmail.com>
2021-10-01 13:43:57 -0300

    Add axe tests for a11y  (#47)
    * Add a11y test for collection page
    
    * Add Product and Home page a11y tests
    
    * Fix hook name
    
    * Fix tests failing
    
    * Apply suggestion to remove test name
    
    * Fix graphql requests path
    
    * Fix A11Y Product page tests

--- src/views/home/Seo/index.tsx
+++ src/views/home/Seo/index.tsx
@@ -1,5 +1,6 @@
 import { GatsbySeo, JsonLd } from 'gatsby-plugin-next-seo'
 import React from 'react'
+import useAddHead from 'src/utils/hooks/useAddHead'
 
 import type { Props as ViewProps } from '../index'
 import { useMetadata } from './hooks/useMetadata'
@@ -10,6 +11,8 @@ export type Props = ViewProps
 function Seo(props: Props) {
   const metadata = useMetadata(props)
   const siteLinksSearchBox = useSiteLinksSearchBoxJsonLd(props)
+
+  useAddHead({ title: metadata.title ?? '' })
 
   return (
     <>
