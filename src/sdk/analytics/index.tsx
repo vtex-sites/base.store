44c49e6fdb814256532ca892e7f1f968bdf3af83
Arthur Andrade <arthurfelandrade@gmail.com>
2021-10-11 11:12:58 -0300

    feat: Add analytics handler
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- src/sdk/analytics/index.tsx
+++ src/sdk/analytics/index.tsx
@@ -1,13 +1,19 @@
 import { useAnalyticsEvent } from '@vtex/store-sdk'
 import type { PropsWithChildren } from 'react'
 
+declare global {
+  interface Window {
+    dataLayer?: any[]
+  }
+}
+
 if (typeof window !== 'undefined') {
-  window.dataLayer = window.dataLayer || []
+  window.dataLayer = window.dataLayer ?? []
 }
 
 export const AnalyticsHandler = ({ children }: PropsWithChildren<unknown>) => {
   useAnalyticsEvent((event) => {
-    window.dataLayer.push(event)
+    window?.dataLayer?.push(event)
   })
 
   return children
