c0afe5e174bdd3e19b3adbca43b0cf30fb6d83a5
Tiago Gimenes <tlgimenes@gmail.com>
2021-10-15 17:21:02 -0300

    adds analytics test

---
+++ cypress/integration/analytics.test.js
@@ -0,0 +1,79 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="cypress" />
+/**
+ * Cypress tests for testing the Analytics module
+ */
+
+import { pages, options } from '../global'
+
+describe('add_to_cart event', () => {
+  beforeEach(() => {
+    cy.clearIDB()
+    cy.clearDataLayer()
+  })
+
+  it('raises add_to_cart at pdp', () => {
+    cy.visit(pages.pdp, options)
+    cy.waitForHydration()
+
+    cy.itemsInCart(0)
+
+    // Add to cart
+    cy.getById('buy-button')
+      .click()
+      .then(($btn) => {
+        cy.itemsInCart(1)
+
+        cy.window().then((window) => {
+          const { dataLayer } = window
+          const skuId = $btn.attr('data-sku')
+
+          expect(dataLayer).to.have.length(1)
+
+          const event = dataLayer.find((e) => e.type === 'add_to_cart')
+
+          expect(event).to.not.be.null
+          expect(event.data).to.have.property('value')
+
+          const item = event.data.items.find((i) => i.item_id === skuId)
+
+          expect(item).to.not.be.null
+          expect(item).to.have.property('currency')
+          expect(item).to.have.property('item_name')
+        })
+      })
+  })
+
+  it('raises add_to_cart at collection page', () => {
+    cy.visit(pages.collection, options)
+    cy.waitForHydration()
+
+    cy.itemsInCart(0)
+
+    // Add to cart
+    cy.getById('buy-button')
+      .first()
+      .click()
+      .then(($btn) => {
+        cy.itemsInCart(1)
+
+        cy.window().then((window) => {
+          const { dataLayer } = window
+          const skuId = $btn.attr('data-sku')
+
+          expect(dataLayer).to.have.length(1)
+
+          const event = dataLayer.find((e) => e.type === 'add_to_cart')
+
+          expect(event).to.not.be.null
+          expect(event.data).to.have.property('value')
+
+          const item = event.data.items.find((i) => i.item_id === skuId)
+
+          expect(item).to.not.be.null
+          expect(item).to.have.property('currency')
+          expect(item).to.have.property('item_name')
+        })
+      })
+  })
+})
