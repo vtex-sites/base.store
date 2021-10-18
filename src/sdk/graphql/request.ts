cb36f4c8fa4ff2cf5697d50bf360e5521c6c86c1
Daniel Zanzini <daniel.zanzini@vtex.com.br>
2021-10-18 11:05:11 -0300

    Removes header definition from base.store

--- src/sdk/graphql/request.ts
+++ src/sdk/graphql/request.ts
@@ -6,16 +6,6 @@ export type RequestOptions = GraphQLRequestOptions
 export const request = async <Query = unknown, Variables = unknown>(
   options: RequestOptions
 ) => {
-  const requestHeaders: HeadersInit = new Headers()
-
-  requestHeaders.set('Content-Type', 'application/json')
-
-  if (options.fetchOptions) {
-    options.fetchOptions.headers = requestHeaders
-  } else {
-    options.fetchOptions = { headers: requestHeaders }
-  }
-
   const { data, errors } = await baseRequest<Variables, Query>(
     '/api/graphql',
     options
