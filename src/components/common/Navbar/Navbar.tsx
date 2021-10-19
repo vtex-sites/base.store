f6529075cf0e8aded132abe825f399f09ef07397
Israel Costa <israel.costa@vtex.com.br>
2021-10-19 13:53:33 -0300

    Add style to Navbar

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -5,9 +5,11 @@ import SearchInput from '../SearchInput'
 import { search } from '../../../sdk/search/utils/search'
 import Navlinks from '../Navlinks'
 
+import './Navbar.module.css'
+
 function Navbar() {
   return (
-    <header className="flex flex-col justify-between items-center bg-primary-400">
+    <header data-store-navbar-header>
       <Navlinks />
       <SearchInput onSubmit={(searchTerm) => search(searchTerm)} />
       <CartToggle />
