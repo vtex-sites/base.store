8d40ed51421f042717af24694f61566503b211c4
Israel Costa <israel.costa@vtex.com.br>
2021-10-21 15:49:38 -0300

    Add search tests

---
+++ cypress/integration/search.test.js
@@ -0,0 +1,40 @@
+// eslint-disable-next-line spaced-comment
+/// <reference types="cypress" />
+/**
+ * Cypress tests for Search Input
+ */
+
+import { pages, options } from '../global'
+
+describe('Search input', () => {
+  beforeEach(() => {
+    cy.clearIDB()
+  })
+
+  context('when search for generic term', () => {
+    it('opens the search page', () => {
+      cy.visit(pages.home, options)
+      cy.waitForHydration()
+
+      cy.getById('store-input').click().type('shirt')
+      cy.getById('store-button').click()
+
+      cy.location('pathname').should((loc) => {
+        expect(loc).to.include(`/s/`)
+      })
+    })
+  })
+
+  context('when search for collection name', () => {
+    it('opens the collection page', () => {
+      cy.visit(pages.home, options)
+      cy.waitForHydration()
+
+      cy.getById('store-input').click().type(pages.brand_name)
+      cy.getById('store-button').click()
+
+      cy.location('pathname')
+      cy.get('#collection-page').should('exist')
+    })
+  })
+})
