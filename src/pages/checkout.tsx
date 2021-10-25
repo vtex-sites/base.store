0172487113f6a0948d924438e6f65ccb2be49eb4
Tiago Gimenes <tlgimenes@gmail.com>
2021-10-25 17:55:35 -0300

    minimal implemetation

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,25 +1,21 @@
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
-import React, { useMemo } from 'react'
+import React from 'react'
 import { useCart } from 'src/sdk/cart/useCart'
 
 const { GATSBY_STORE_ID: account, GATSBY_VTEX_ENVIRONMENT: environment } =
   process.env
 
-const useCheckoutUrl = (cartId: string) =>
-  useMemo(
-    () =>
-      `https://${account}.${environment}.com.br/checkout?orderFormId=${cartId}`,
-    [cartId]
-  )
-
 function Page() {
   const { id } = useCart()
-  const src = useCheckoutUrl(id)
 
   return (
     <>
       <GatsbySeo nofollow noindex />
-      <iframe title="checkout" src={src} className="min-h-screen min-w-full" />
+      <iframe
+        title="checkout"
+        src={`https://${account}.${environment}.com.br/checkout?orderFormId=${id}`}
+        className="min-h-screen min-w-full"
+      />
     </>
   )
 }
