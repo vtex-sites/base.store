4a9a5c29f31173d9e6d77b6344b930f6fd67a77d
Tiago Gimenes <tlgimenes@gmail.com>
2021-10-25 17:54:41 -0300

    support vtex only

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -2,22 +2,15 @@ import { GatsbySeo } from 'gatsby-plugin-next-seo'
 import React, { useMemo } from 'react'
 import { useCart } from 'src/sdk/cart/useCart'
 
-const {
-  GATSBY_COMMERCE_PLATFORM: platform = 'vtex',
-  GATSBY_STORE_ID: account,
-  GATSBY_VTEX_ENVIRONMENT: environment,
-} = process.env
+const { GATSBY_STORE_ID: account, GATSBY_VTEX_ENVIRONMENT: environment } =
+  process.env
 
 const useCheckoutUrl = (cartId: string) =>
-  useMemo(() => {
-    switch (platform) {
-      case 'vtex':
-        return `https://${account}.${environment}.com.br/checkout?orderFormId=${cartId}`
-
-      default:
-        throw new Error(`Uknown platform: ${platform}`)
-    }
-  }, [cartId])
+  useMemo(
+    () =>
+      `https://${account}.${environment}.com.br/checkout?orderFormId=${cartId}`,
+    [cartId]
+  )
 
 function Page() {
   const { id } = useCart()
