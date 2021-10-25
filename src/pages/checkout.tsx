be05f41d70334e9f7df296e9bb3b954af4602b96
Tiago Gimenes <tlgimenes@gmail.com>
2021-10-25 17:33:31 -0300

    improve readability

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -1,27 +1,24 @@
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
-import React from 'react'
+import React, { useMemo } from 'react'
 import { useCart } from 'src/sdk/cart/useCart'
 
 const platform = process.env.GATSBY_COMMERCE_PLATFORM ?? 'vtex'
 const storeId = process.env.GATSBY_STORE_ID
 const environment = process.env.GATSBY_VTEX_ENVIRONMENT
 
-const getCheckoutUrl = {
+const urlsByPlatform: Record<string, (id: string) => string> = {
   vtex: (cartId: string) =>
     `https://${storeId}.${environment}.com.br/checkout?orderFormId=${cartId}`,
-}[platform]
+}
 
 function Page() {
   const { id } = useCart()
+  const src = useMemo(() => urlsByPlatform[platform]?.(id), [id])
 
   return (
     <>
       <GatsbySeo nofollow noindex />
-      <iframe
-        title="checkout"
-        src={getCheckoutUrl?.(id)}
-        className="min-h-screen min-w-full"
-      />
+      <iframe title="checkout" src={src} className="min-h-screen min-w-full" />
     </>
   )
 }
