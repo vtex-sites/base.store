e58e33a5f58c25d2ff6935ddd4337b9ece004d5c
Larícia Mota <laricia.mota@vtex.com.br>
2021-10-25 19:25:41 -0300

    Update version

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button, Checkbox, Form, Input, Label } from '@vtex/store-ui'
 
 function Footer() {
-  return <footer className="w-full h-32 bg-primary-400">Footer</footer>
+  const [email, setEmail] = useState<string>()
+
+  return (
+    <footer className="w-full h-32 bg-primary-400">
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault()
+          // eslint-disable-next-line no-alert
+          alert(`Thank you for subscribing ${email}`)
+        }}
+      >
+        <h1>Sign up & save 15% off your first order</h1>
+        <h2>
+          Be the first to hear about special offers, new product launches, gift
+          ideas and more.
+        </h2>
+        <div>
+          <Label htmlFor="email">Email:</Label>
+          <Input
+            id="email"
+            placeholder="Enter your email"
+            required
+            onChange={(e) => setEmail(e.currentTarget.value)}
+          />
+        </div>
+        <div>
+          <Label>
+            <Checkbox required />I agree to receive emails from Brand. View our{' '}
+            <a href="/">Privacy Policy</a>.
+          </Label>
+        </div>
+        <Button type="submit">Subscribe</Button>
+      </Form>{' '}
+      ...Footer
+    </footer>
+  )
 }
 
 export default Footer
--- src/views/Layout.tsx
+++ src/views/Layout.tsx
@@ -1,17 +1,15 @@
-import React, { lazy, Suspense, useState } from 'react'
+import React, { lazy, Suspense } from 'react'
 import Footer from 'src/components/common/Footer'
 import Navbar from 'src/components/common/Navbar'
 import { useCartNotificationEffect } from 'src/sdk/cart/useCartNotificationEffect'
 import { useUI } from 'src/sdk/ui'
 import type { PropsWithChildren } from 'react'
-import { Button, Checkbox, Form, Input, Label } from '@vtex/store-ui'
 
 const CartSidebar = lazy(() => import('src/components/cart/CartSidebar'))
 const Toast = lazy(() => import('src/components/ui/Toast'))
 
 function Layout({ children }: PropsWithChildren<unknown>) {
   const { displayMinicart, toasts } = useUI()
-  const [email, setEmail] = useState<string>()
 
   useCartNotificationEffect()
 
@@ -19,35 +17,6 @@ function Layout({ children }: PropsWithChildren<unknown>) {
     <>
       <Navbar />
       <main className="min-h-screen">{children}</main>
-      <Form
-        onSubmit={(e) => {
-          e.preventDefault()
-          // eslint-disable-next-line no-alert
-          alert(`Thank you for subscribing ${email}`)
-        }}
-      >
-        <h1>Sign up & save 15% off your first order</h1>
-        <h2>
-          Be the first to hear about special offers, new product launches, gift
-          ideas and more.
-        </h2>
-        <div>
-          <Label htmlFor="email">Email:</Label>
-          <Input
-            id="email"
-            placeholder="Enter your email"
-            required
-            onChange={(e) => setEmail(e.currentTarget.value)}
-          />
-        </div>
-        <div>
-          <Label>
-            <Checkbox required />I agree to receive emails from Brand. View our{' '}
-            <a href="/">Privacy Policy</a>.
-          </Label>
-        </div>
-        <Button type="submit">Subscribe</Button>
-      </Form>
       <Footer />
       {displayMinicart && (
         <Suspense fallback={null}>
