c7c24ef43a3588bb90ec4305c19d790a62e7bc7b
Tiago Gimenes <tlgimenes@gmail.com>
2021-10-25 17:43:45 -0300

    inline variables

--- src/pages/checkout.tsx
+++ src/pages/checkout.tsx
@@ -3,12 +3,10 @@ import React, { useMemo } from 'react'
 import { useCart } from 'src/sdk/cart/useCart'
 
 const platform = process.env.GATSBY_COMMERCE_PLATFORM ?? 'vtex'
-const storeId = process.env.GATSBY_STORE_ID
-const environment = process.env.GATSBY_VTEX_ENVIRONMENT
 
 const urlsByPlatform: Record<string, (id: string) => string> = {
   vtex: (cartId: string) =>
-    `https://${storeId}.${environment}.com.br/checkout?orderFormId=${cartId}`,
+    `https://${process.env.GATSBY_STORE_ID}.${process.env.GATSBY_VTEX_ENVIRONMENT}.com.br/checkout?orderFormId=${cartId}`,
 }
 
 function Page() {
