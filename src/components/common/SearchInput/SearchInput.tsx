520292c62fa8c3571936a6d50534d9ad3035bca6
Israel Costa <israel.costa@vtex.com.br>
2021-11-05 15:22:24 -0300

    Send search term in sendAnalyticsEvent

--- src/components/common/SearchInput/SearchInput.tsx
+++ src/components/common/SearchInput/SearchInput.tsx
@@ -2,13 +2,23 @@ import React from 'react'
 import { navigate } from 'gatsby'
 import type { SearchInputProps as UISearchInputProps } from '@faststore/ui'
 import { SearchInput as UISearchInput } from '@faststore/ui'
-import { initSearchParamsState, formatSearchParamsState } from '@faststore/sdk'
+import type { SearchEvent } from '@faststore/sdk'
+import {
+  initSearchParamsState,
+  formatSearchParamsState,
+  sendAnalyticsEvent,
+} from '@faststore/sdk'
 
 import './SearchInput.module.css'
 
 declare type SearchInputProps = Omit<UISearchInputProps, 'onSubmit'>
 
 const search = async (term: string) => {
+  sendAnalyticsEvent<SearchEvent>({
+    type: 'search',
+    data: { search_term: term },
+  })
+
   const searchParamsState = initSearchParamsState({
     term,
     base: '/s',
