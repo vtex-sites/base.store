e5ae7c7a672bba0271d887cda09f3911e7fadb6e
Tiago Gimenes <tlgimenes@gmail.com>
2021-11-10 14:38:09 -0300

    make indices work

--- src/components/product/ProductGrid/ProductGrid.tsx
+++ src/components/product/ProductGrid/ProductGrid.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import type { ComponentPropsWithoutRef } from 'react'
-import { useSearch } from 'src/sdk/search/useSearch'
 
 import ProductSummary from '../ProductSummary'
 
@@ -10,28 +9,22 @@ interface Props {
       node: ComponentPropsWithoutRef<typeof ProductSummary>['product']
     }>
   }
+  page: number
+  pageSize: number
 }
 
-function ProductGrid({ products }: Props) {
-  const {
-    searchParams: { page },
-    pageInfo: { size },
-  } = useSearch()
-
+function ProductGrid({ products, page, pageSize }: Props) {
   return (
     <div className="grid grid-cols-2 gap-2 mb-2 sm:grid-cols-4 sm:gap-7 sm:mb-7">
       {products.edges.map(({ node: product }, idx) => (
         <ProductSummary
           key={`${product.id}`}
           product={product}
-          index={getProductIndex(size, page, idx)}
+          index={pageSize * page + idx + 1}
         />
       ))}
     </div>
   )
 }
-
-const getProductIndex = (size: number, page: number, idx: number) =>
-  size * page + idx + 1
 
 export default ProductGrid
