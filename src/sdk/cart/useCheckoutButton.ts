8ddf0ce1b8322e89188ea76c31d565c40563c34c
Ícaro Azevedo <icazevedo10@gmail.com>
2021-11-30 13:24:14 -0300

    Validating checkout on subdomain (#117)
    * Adding random change so I can open a PR
    
    * Updating site url
    
    * Adding go to checkout link
    
    * Using vtex.env and removing custom button
    
    * Fixing env variable name (checkout url)

--- src/sdk/cart/useCheckoutButton.ts
+++ src/sdk/cart/useCheckoutButton.ts
@@ -2,6 +2,9 @@ import { useCart } from './useCart'
 
 const storeId = process.env.GATSBY_STORE_ID
 const environment = process.env.GATSBY_VTEX_ENVIRONMENT
+const checkoutURL =
+  process.env.GATSBY_CHECKOUT_URL ??
+  `https://${storeId}.${environment}.com.br/checkout`
 
 export const useCheckoutButton = () => {
   const { isValidating, id } = useCart()
@@ -10,7 +13,7 @@ export const useCheckoutButton = () => {
     e.preventDefault()
 
     if (!isValidating && id) {
-      window.location.href = `https://${storeId}.${environment}.com.br/checkout?orderFormId=${id}`
+      window.location.href = `${checkoutURL}?orderFormId=${id}`
     }
   }
 
