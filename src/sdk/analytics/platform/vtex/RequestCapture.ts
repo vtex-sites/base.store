27619bae095475188fef3006acb29b1e1e5880e4
igorbrasileiro <brasileiro456@gmail.com>
2021-12-01 12:18:33 -0300

    Enhance the RequestCapture script

--- src/sdk/analytics/platform/vtex/RequestCapture.ts
+++ src/sdk/analytics/platform/vtex/RequestCapture.ts
@@ -1,5 +1,13 @@
 import type { AnalyticsEvent } from '@faststore/sdk'
 
 export function sendRCEvent(event: AnalyticsEvent) {
-  window.vtexrca('sendevent', event.type, event)
+  // This runs if the rc.js script hasn't loaded yet.
+  if (!window.NavigationCapture) {
+    window.vtexrca = {
+      q: [],
+    }
+    window.vtexrca.q.push('sendevent', event.type, event)
+  }
+
+  window.NavigationCapture.sendEvent(event.type, event)
 }
