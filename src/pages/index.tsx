da9c05ed7736c0d9e97164aa1622971d4c4e8d29
Tiago Gimenes <tlgimenes@gmail.com>
2021-12-02 16:17:40 -0300

    chore: Simplify home page (#123)
    * simplify home page
    
    * add comment disclaimer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,59 @@
+import { useSession } from '@faststore/sdk'
 import { graphql } from 'gatsby'
+import { GatsbySeo, JsonLd } from 'gatsby-plugin-next-seo'
 import React from 'react'
-import View from 'src/views/home'
 import type { PageProps } from 'gatsby'
 import type { HomePageQueryQuery } from '@generated/graphql'
 
 export type Props = PageProps<HomePageQueryQuery>
 
 function Page(props: Props) {
-  return <View {...props} />
+  const {
+    data: { site },
+    location: { pathname, host },
+  } = props
+
+  const { locale } = useSession()
+
+  const title = site?.siteMetadata?.title ?? ''
+  const siteUrl = `https://${host}${pathname}`
+
+  return (
+    <>
+      {/* SEO */}
+      <GatsbySeo
+        title={title}
+        description={site?.siteMetadata?.description ?? ''}
+        titleTemplate={site?.siteMetadata?.titleTemplate ?? ''}
+        language={locale}
+        canonical={siteUrl}
+        openGraph={{
+          type: 'website',
+          url: siteUrl,
+          title: title ?? '',
+          description: site?.siteMetadata?.description ?? '',
+        }}
+      />
+      <JsonLd
+        json={{
+          '@context': 'https://schema.org',
+          '@type': 'WebSite',
+          url: siteUrl,
+          potentialAction: {
+            '@type': 'SearchAction',
+            target: `${siteUrl}/s/?q={search_term_string}`,
+            'query-input': 'required name=search_term_string',
+          },
+        }}
+      />
+
+      {/*
+        Sections: Components imported from '../components/sections' only.
+        Do not import or render components from any other folder in here.
+      */}
+      <h1>{title}</h1>
+    </>
+  )
 }
 
 export const query = graphql`
