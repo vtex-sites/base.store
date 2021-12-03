c88ac0d76521d767ee72e3777e38c490f0001ddd
igorbrasileiro <brasileiro456@gmail.com>
2021-12-03 17:57:52 -0300

    Add mocked event to test performance

--- src/sdk/analytics/platform/vtex/IntelligentSearch.ts
+++ src/sdk/analytics/platform/vtex/IntelligentSearch.ts
@@ -2,13 +2,14 @@ import RecSys from '@biggy/recsys'
 import { SessionCartAddEvent } from '@biggy/recsys/event/types/session/cart-add'
 import { SessionCartRemoveEvent } from '@biggy/recsys/event/types/session/cart-remove'
 // import { PageCartEvent } from '@biggy/recsys/event/types/page/cart'
-// import { DataLayerPageCategoryEvent } from '@biggy/recsys/event/types/datalayer/datalayer-page-category'
+import { DataLayerPageCategoryEvent } from '@biggy/recsys/event/types/datalayer/datalayer-page-category'
 // import { DataLayerPageCategoryClickEvent } from '@biggy/recsys/event/types/datalayer/datalayer-page-category-click'
 // import { PageConfirmationEvent } from '@biggy/recsys/event/types/page/confirmation'
 // import { DataLayerSearchQueryEvent } from '@biggy/recsys/event/types/datalayer/datalayer-search-query'
 // import { DataLayerSearchClickEvent } from '@biggy/recsys/event/types/datalayer/datalayer-search-click'
 import { DataLayerProductViewEvent } from '@biggy/recsys/event/types/datalayer/datalayer-product-view'
 import type { ViewItemEvent } from 'src/sdk/product/useProductLink'
+import type { ViewItemListEvent } from '@faststore/sdk'
 
 import type { AnalyticsEvent } from '../..'
 import type { VTEXAddToCartEvent, VTEXRemoveFromCartEvent } from '../../types'
@@ -16,7 +17,7 @@ import type { VTEXAddToCartEvent, VTEXRemoveFromCartEvent } from '../../types'
 const storeId = process.env.GATSBY_STORE_ID
 const channel = process.env.GATSBY_VTEX_CHANNEL
 // TODO: add the right host
-const allowedHosts = ['localhost:8000']
+const allowedHosts = ['base.vtex.app', 'localhost:9000', 'localhost:8000']
 
 if (
   typeof window !== undefined &&
@@ -41,8 +42,18 @@ const productView = (event: ViewItemEvent) => {
   }).push()
 }
 
+const itemListView = (_: ViewItemListEvent) => {
+  // TODO: Mocked only to performance test
+  return new DataLayerPageCategoryEvent({
+    text: 'women',
+    match: 17,
+    locale: '',
+  }).push()
+}
+
 const addToCart = (event: VTEXAddToCartEvent) => {
-  const locale = '' // TODO
+  // TODO: Get the right locale
+  const locale = ''
   const [item] = event.data.items
 
   return new SessionCartAddEvent(channel, locale)
@@ -57,6 +68,7 @@ const addToCart = (event: VTEXAddToCartEvent) => {
 }
 
 const removeFromCart = (event: VTEXRemoveFromCartEvent) => {
+  // TODO: Get the right locale
   const locale = ''
   const [item] = event.data.items
 
@@ -75,6 +87,10 @@ export const sendISEvent = (event: AnalyticsEvent) => {
   switch (event.type) {
     case 'view_item':
       productView(event)
+      break
+
+    case 'view_item_list':
+      itemListView(event)
       break
 
     case 'add_to_cart':
--- src/sdk/analytics/platform/vtex/index.ts
+++ src/sdk/analytics/platform/vtex/index.ts
@@ -3,6 +3,7 @@ import { sendISEvent } from './IntelligentSearch'
 import { sendRCEvent } from './RequestCapture'
 
 export default function sendVTEXEvents(event: AnalyticsEvent) {
-  sendRCEvent(event)
-  sendISEvent(event)
+  // Use setTimeout to break those sendEvents into small tasks
+  setTimeout(() => sendRCEvent(event), 0)
+  setTimeout(() => sendISEvent(event), 0)
 }
