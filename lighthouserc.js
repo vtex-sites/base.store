80a002870745fe9d906b65570438f8acf283f30e
Tiago Gimenes <tlgimenes@gmail.com>
2021-12-03 16:14:40 -0300

    fix wrong lh config

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,8 +3,8 @@ const VTEXLHConfig = require('@vtex/lighthouse-config').default
 const { lighthouse: lh } = require('./store.config')
 
 module.exports = VTEXLHConfig({
-  urls: lh.pages,
-  server: Object.values(lh.server),
+  urls: Object.values(lh.pages),
+  server: lh.server,
   assertions: {
     'csp-xss': 'off',
     'legacy-javascript': ['error', { maxLength: 1 }],
