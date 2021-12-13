b4fc7ffed21fd5937d1cfe9b25fa4c70029bdcbe
Fanny <fannychienn@gmail.com>
2021-12-13 19:36:23 -0300

    style input search default

--- src/pages/pattern-library.tsx
+++ src/pages/pattern-library.tsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Button as UIButton } from '@faststore/ui'
 
+import SearchInput from '../components/common/SearchInput'
+
 import '../styles/pattern-library.scss'
 
 function Page() {
@@ -58,6 +60,15 @@ function Page() {
           </li>
           <li>
             <UIButton>Call to Action</UIButton>
+          </li>
+        </ul>
+      </section>
+
+      <section className="grid-section grid-content">
+        <h2 className="title-subsection">Inputs</h2>
+        <ul className="list-horizontal">
+          <li>
+            <SearchInput />
           </li>
         </ul>
       </section>
