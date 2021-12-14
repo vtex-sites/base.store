6e6308a7a4ad2fc380aa2f4f8dffc938e39c344b
Guilherme Carvalho <guilherme@guava.com.br>
2021-12-14 16:32:29 -0300

    Take 2

--- src/html.jsx
+++ src/html.jsx
@@ -14,12 +14,6 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="yes"
-        />
         <style>{cssImport}</style>
         {props.headComponents}
       </head>
