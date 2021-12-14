98c283380d4955a63b0032733bf017d71d9534af
Fanny <fannychienn@gmail.com>
2021-12-14 20:18:55 -0300

    attempt to get rid of ssr window issue

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react'
+import React, { useEffect, useState, lazy, Suspense } from 'react'
 import Footer from 'src/components/common/Footer'
 import Navbar from 'src/components/common/Navbar'
 import { useCartNotificationEffect } from 'src/sdk/cart/useCartNotificationEffect'
