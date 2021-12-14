9493edfe083ca66e98a418b201167e8fcf97db2d
eduardoformiga <eduardo.formiga@vtex.com.br>
2021-12-14 20:47:48 -0300

    Render pattern library without the default layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,7 +4,6 @@ import Navbar from 'src/components/common/Navbar'
 import { useCartNotificationEffect } from 'src/sdk/cart/useCartNotificationEffect'
 import { useUI } from 'src/sdk/ui'
 import type { PropsWithChildren } from 'react'
-import { useLocation } from '@reach/router'
 
 const CartSidebar = lazy(() => import('src/components/cart/CartSidebar'))
 const Toast = lazy(() => import('src/components/ui/Toast'))
@@ -13,14 +12,6 @@ function Layout({ children }: PropsWithChildren<unknown>) {
   const { displayMinicart, toasts } = useUI()
 
   useCartNotificationEffect()
-
-  // Temporary added condition for pattern library page
-  /* FIXME Remove this after removing pattern library page */
-  const location = useLocation()
-
-  if (location.pathname === '/pattern-library') {
-    return <>{children}</>
-  }
 
   return (
     <>
