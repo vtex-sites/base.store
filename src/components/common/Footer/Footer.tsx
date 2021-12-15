9da88385a0cd8cd18293583809fde5803a9f0e4e
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2021-12-15 14:03:51 -0300

    Add icons (#143)
    * Add phosphor-react
    
    * Add social icons to footer

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,7 +1,21 @@
 import React from 'react'
+import {
+  FacebookLogo,
+  InstagramLogo,
+  PinterestLogo,
+  TwitterLogo,
+} from 'phosphor-react'
 
 function Footer() {
-  return <footer>Footer</footer>
+  return (
+    <footer>
+      Follow us
+      <FacebookLogo />
+      <InstagramLogo />
+      <PinterestLogo />
+      <TwitterLogo />
+    </footer>
+  )
 }
 
 export default Footer
