1be6becd8fa05bc5f59935fd9de24ffb4d61b06e
Arthur Andrade <arthurfelandrade@gmail.com>
2021-12-16 11:35:15 -0300

    feat: Add data-store-button-cart
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- src/components/cart/CartToggle/CartToggle.tsx
+++ src/components/cart/CartToggle/CartToggle.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import Button from 'src/components/ui/Button'
 import { useCartToggleButton } from 'src/sdk/cart/useCartToggleButton'
 
-import * as styles from './cart-toggle.module.scss'
+import './cart-toggle.scss'
 
 function CartToggle() {
   const btnProps = useCartToggleButton()
 
   return (
-    <Button {...btnProps} className={styles.buttonCart}>
+    <Button {...btnProps} data-store-button-cart>
       <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="" />
     </Button>
   )
