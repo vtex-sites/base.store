76f186a14c9de2c06dd8bbc560fe618bbbb5d98a
Arthur Andrade <arthurfelandrade@gmail.com>
2021-12-16 13:56:11 -0300

    feat: Add icon and css variables
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- src/components/cart/CartToggle/CartToggle.tsx
+++ src/components/cart/CartToggle/CartToggle.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Button from 'src/components/ui/Button'
 import { useCartToggleButton } from 'src/sdk/cart/useCartToggleButton'
+import { ShoppingCart } from 'phosphor-react'
 
 import './cart-toggle.scss'
 
@@ -8,8 +9,8 @@ function CartToggle() {
   const btnProps = useCartToggleButton()
 
   return (
-    <Button {...btnProps} data-store-button-cart>
-      <img src="https://cdn-icons-png.flaticon.com/512/263/263142.png" alt="" />
+    <Button {...btnProps} className="cart-toggle">
+      <ShoppingCart />
     </Button>
   )
 }
