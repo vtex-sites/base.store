d245655d1ad681a66c4a95db43ba9b3a6c526ff9
Fanny Chien <fannychienn@gmail.com>
2021-12-20 18:39:27 -0300

    Feat: style Badge component  (#157)
    * Styles badge component
    
    * Fixes syntax
    
    * Updates styles on pattern library
    
    * Fixes comment regards to other colors

--- src/pages/pattern-library.tsx
+++ src/pages/pattern-library.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
-import { Button as UIButton } from '@faststore/ui'
+import { Button as UIButton, Badge as UIBadge } from '@faststore/ui'
 import { BellRinging } from 'phosphor-react'
 
 import Alert from '../components/ui/Alert'
@@ -130,6 +130,18 @@ function Page() {
                     Show Alert
                   </UIButton>
                 )}
+              </li>
+            </ul>
+          </section>
+
+          <section className="grid-section grid-content">
+            <h2 className="title-subsection">Badge</h2>
+            <ul className="list-horizontal">
+              <li>
+                <UIBadge>15% off</UIBadge>
+              </li>
+              <li>
+                <UIBadge className="badge_small">15% off</UIBadge>
               </li>
             </ul>
           </section>
