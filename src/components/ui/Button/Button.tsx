1e94f6f70b252d617c2ae28789744a89a2196a54
Fanny <fannychienn@gmail.com>
2021-12-21 17:18:35 -0300

    Extracts button styles

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -2,10 +2,12 @@ import React from 'react'
 import { Button as UIButton } from '@faststore/ui'
 import type { ButtonProps } from '@faststore/ui'
 
+import './buttons.scss'
+
 type Props = ButtonProps
 
 function Button(props: Props) {
-  return <UIButton {...props} />
+  return <UIButton className="button" {...props} />
 }
 
 export default Button
