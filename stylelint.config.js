aeaf492a2b285cf8cf2b6225dc54348c86e61228
Fanny <fannychienn@gmail.com>
2021-12-21 21:51:00 -0300

    Attempts to update selector pattern

--- stylelint.config.js
+++ stylelint.config.js
@@ -77,5 +77,7 @@ module.exports = {
     'scss/operator-no-unspaced': true,
     'scss/selector-no-redundant-nesting-selector': true,
     'scss/at-rule-no-unknown': true,
+    'selector-class-pattern':
+      '^(?:(?:o|c|u|t|s|is|has|_|js|qa)-)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*(?:__[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:--[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*)?(?:\\[.+\\])?$',
   },
 }
