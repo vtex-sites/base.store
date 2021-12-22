d396efde9ec9733031e6428ac96724add670248b
Eduardo Formiga <eduardo.formiga@gmail.com>
2021-12-21 22:28:57 -0300

    feat: Tiles component (#141)
    * Scaffolds Tiles component
    
    * Fixes child validation
    
    * Aims to avoid magic numbers
    
    * Adds as operator type assertion
    
    * Trigger CI
    
    * Refactors how to render children and its css
    
    * Aims to valid Tiles only in dev mode

---
+++ src/components/ui/Tiles/Tile.tsx
@@ -0,0 +1,23 @@
+import React, { forwardRef } from 'react'
+import type { HTMLAttributes } from 'react'
+
+export interface TileProps extends HTMLAttributes<HTMLDivElement> {
+  /**
+   * ID to find this component in testing tools (e.g.: cypress,
+   * testing-library, and jest).
+   */
+  testId?: string
+}
+
+const Tile = forwardRef<HTMLDivElement, TileProps>(function Tile(
+  { testId = 'store-tile', children, ...otherProps },
+  ref
+) {
+  return (
+    <div ref={ref} data-store-tile data-testid={testId} {...otherProps}>
+      {children}
+    </div>
+  )
+})
+
+export default Tile
