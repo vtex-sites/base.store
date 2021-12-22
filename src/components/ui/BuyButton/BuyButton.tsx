58ff74325f45bcb54e0bd266e7b1977b09960fdf
renatamottam <renatamotta.m@gmail.com>
2021-12-22 09:56:29 -0300

    Fixes ShoppingCartIcon naming

--- src/components/ui/BuyButton/BuyButton.tsx
+++ src/components/ui/BuyButton/BuyButton.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Button as UIButton } from '@faststore/ui'
 import type { ButtonProps } from '@faststore/ui'
-import { ShoppingCart } from 'phosphor-react'
+import { ShoppingCart as ShoppingCartIcon } from 'phosphor-react'
 
 type Props = ButtonProps
 
 function BuyButton({ children, ...props }: Props) {
   return (
     <UIButton {...props}>
-      <ShoppingCart />
+      <ShoppingCartIcon />
       {children}
     </UIButton>
   )
