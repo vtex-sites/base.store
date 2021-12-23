0f92d6c6508ed3d7cea3337d119a0cdd747993e1
Fanny <fannychienn@gmail.com>
2021-12-23 16:00:42 -0300

    Attempts to fix plp infinite scroll tests

--- cypress/integration/plp.test.js
+++ cypress/integration/plp.test.js
@@ -126,6 +126,9 @@ describe('Infinite Scroll pagination', () => {
           .should('exist')
           .click()
           .then(() => {
+            // Ensure wait new page after clicks show more
+            cy.location('search').should('match', /\page=1$/)
+
             // The skuId of the last product on the page
             let skuIdBeforeNavigate
 
@@ -169,17 +172,13 @@ describe('Infinite Scroll pagination', () => {
         cy.getById('product-link')
           .last()
           .scrollIntoView()
-          .location()
-          .should(($loc) => {
-            expect($loc.search).includes('page=1')
-          })
+          .location('search')
+          .should('match', /\page=1$/)
           .getById('product-link')
           .first()
-          .scrollIntoView()
-          .location()
-          .should(($loc) => {
-            expect($loc.search).includes('page=0')
-          })
+          .scrollIntoView({ offset: { top: -10 } })
+          .location('search')
+          .should('match', /\page=0$/)
       })
   })
 })
