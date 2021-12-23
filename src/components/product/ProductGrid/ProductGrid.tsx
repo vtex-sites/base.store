6480b48614d4305f85f90bccf46a05e15ecd281b
Fanny <fannychienn@gmail.com>
2021-12-22 21:01:47 -0300

    Adds missing heading h2

--- src/components/product/ProductGrid/ProductGrid.tsx
+++ src/components/product/ProductGrid/ProductGrid.tsx
@@ -11,14 +11,20 @@ interface Props {
 
 function ProductGrid({ products, page, pageSize }: Props) {
   return (
-    /* FIXME Remove this once grid page are styled properly */
-    <ul className="temp-grid-row">
-      {products.map((product, idx) => (
-        <li key={`${product.id}`}>
-          <ProductSummary product={product} index={pageSize * page + idx + 1} />
-        </li>
-      ))}
-    </ul>
+    <>
+      <h2>Most Wanted</h2>
+      {/* FIXME Remove this once grid page are styled properly */}
+      <ul className="temp-grid-row">
+        {products.map((product, idx) => (
+          <li key={`${product.id}`}>
+            <ProductSummary
+              product={product}
+              index={pageSize * page + idx + 1}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
 
