83e7b39281edd7023cf461b2941f25fa6c411f48
igorbrasileiro <brasileiro456@gmail.com>
2021-12-23 11:27:53 -0300

    Commit to be removed - only test

--- src/sdk/analytics/index.tsx
+++ src/sdk/analytics/index.tsx
@@ -1,10 +1,6 @@
 import { useAnalyticsEvent } from '@faststore/sdk'
 import type { PropsWithChildren } from 'react'
 
-if (typeof window !== 'undefined') {
-  window.dataLayer = window.dataLayer ?? []
-}
-
 export const AnalyticsHandler = ({ children }: PropsWithChildren<unknown>) => {
   useAnalyticsEvent((event) => {
     window.dataLayer.push({ event: event.name, ecommerce: event.params })
