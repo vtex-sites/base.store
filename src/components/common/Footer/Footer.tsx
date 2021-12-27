bc79db284d3647277e78af16ce7ce39e7b3e8a7c
Sara Lins <saranicoly0@gmail.com>
2021-12-27 12:18:37 -0300

    add list of incentives

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,7 +1,52 @@
 import React from 'react'
+import { Incentive, List } from '@faststore/ui'
+
+import {
+  QualityIcon,
+  SafetyIcon,
+  GuaranteeIcon,
+  ShippingIcon,
+  StoreIcon,
+} from './Icons'
 
 function Footer() {
-  return <footer className="w-full h-32 bg-primary-400">Footer</footer>
+  return (
+    <footer className="w-full h-32 bg-primary-200">
+      {/* list of incentives */}
+      <List variant="unordered" style={{ display: 'inline-flex' }}>
+        <li>
+          <Incentive>
+            <SafetyIcon />
+            <span>Trusted by SafeCon</span>
+          </Incentive>
+        </li>
+        <li>
+          <Incentive>
+            <QualityIcon />
+            <span>Quality Products</span>
+          </Incentive>
+        </li>
+        <li>
+          <Incentive>
+            <GuaranteeIcon />
+            <span>3-years Guarantee</span>
+          </Incentive>
+        </li>
+        <li>
+          <Incentive>
+            <StoreIcon />
+            <span>Pickup Options</span>
+          </Incentive>
+        </li>
+        <li>
+          <Incentive>
+            <ShippingIcon />
+            <span>Free Shipping</span>
+          </Incentive>
+        </li>
+      </List>
+    </footer>
+  )
 }
 
 export default Footer
