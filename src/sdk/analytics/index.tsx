052a295748328f54b3f0856a77479d0bfbb48908
igorbrasileiro <brasileiro456@gmail.com>
2021-12-28 15:12:44 -0300

    Fix cypress tests

--- src/sdk/analytics/index.tsx
+++ src/sdk/analytics/index.tsx
@@ -1,6 +1,10 @@
 import { useAnalyticsEvent } from '@faststore/sdk'
 import type { PropsWithChildren } from 'react'
 
+if (typeof window !== 'undefined') {
+  window.dataLayer = window.dataLayer ?? []
+}
+
 export const AnalyticsHandler = ({ children }: PropsWithChildren<unknown>) => {
   useAnalyticsEvent((event) => {
     window.dataLayer.push({ event: event.name, ecommerce: event.params })
