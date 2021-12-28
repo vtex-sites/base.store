bae6beb23f75406a6db6033e63b4a4e77cfa7b11
igorbrasileiro <brasileiro456@gmail.com>
2021-12-28 13:43:07 -0300

    Add copy partytown scripts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,6 +1,5 @@
-// const path = require('path')
-
-// const CopyPlugin = require('copy-webpack-plugin')
+const fs = require('fs')
+const path = require('path')
 
 exports.onCreateWebpackConfig = ({ actions: { setWebpackConfig }, stage }) => {
   const profiling = process.env.GATSBY_STORE_PROFILING === 'true'
@@ -16,25 +15,9 @@ exports.onCreateWebpackConfig = ({ actions: { setWebpackConfig }, stage }) => {
     })
   }
 
-  // TODO: Copy files from partytown
-  // setWebpackConfig({
-  // plugins: [
-  // new CopyPlugin({
-  // patterns: [
-  // {
-  // from: path.join(
-  // __dirname,
-  // 'node_modules',
-  // '@builder.io',
-  // 'partytown',
-  // 'lib'
-  // ),
-  // to: path.join(__dirname, 'public', '~partytown'),
-  // },
-  // ],
-  // }),
-  // ],
-  // })
+  if (stage === 'develop') {
+    copyPartytown()
+  }
 }
 
 exports.onCreateBabelConfig = ({ actions }) => {
@@ -42,4 +25,39 @@ exports.onCreateBabelConfig = ({ actions }) => {
     name: `@vtex/graphql-utils/babel`,
     options: {},
   })
+}
+
+const copyRecursiveSync = (src, dest) => {
+  const exists = fs.existsSync(src)
+  const stats = exists && fs.statSync(src)
+  const isDirectory = exists && stats.isDirectory()
+  const destinationExists = fs.existsSync(dest)
+
+  if (destinationExists) {
+    return
+  }
+
+  if (isDirectory) {
+    fs.mkdirSync(dest)
+    fs.readdirSync(src).forEach((childItemName) => {
+      copyRecursiveSync(
+        path.join(src, childItemName),
+        path.join(dest, childItemName)
+      )
+    })
+  } else {
+    fs.copyFileSync(src, dest)
+  }
+}
+
+const copyPartytown = () => {
+  copyRecursiveSync(
+    path.join(__dirname, 'node_modules/@builder.io/partytown/lib'),
+    path.join(__dirname, 'static/~partytown')
+  )
+}
+
+exports.onPreBuild = ({ reporter }) => {
+  reporter.info('Copying Partytown Files')
+  copyPartytown()
 }
