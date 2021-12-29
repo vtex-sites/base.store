549ed1972aaada804f8c7bef419d5e123dc0ff1e
renatamottam <renatamotta.m@gmail.com>
2021-12-29 10:00:29 -0300

    Adds Alert to Layout

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,12 @@
 import React, { lazy, Suspense } from 'react'
 import Footer from 'src/components/common/Footer'
 import Navbar from 'src/components/common/Navbar'
+import { BellRinging as BellRingingIcon } from 'phosphor-react'
 import { useCartNotificationEffect } from 'src/sdk/cart/useCartNotificationEffect'
 import { useUI } from 'src/sdk/ui'
 import type { PropsWithChildren } from 'react'
+
+import Alert from './components/ui/Alert'
 
 const CartSidebar = lazy(() => import('src/components/cart/CartSidebar'))
 const Toast = lazy(() => import('src/components/ui/Toast'))
@@ -15,14 +18,21 @@ function Layout({ children }: PropsWithChildren<unknown>) {
 
   return (
     <>
+      <Alert icon={<BellRingingIcon size={24} />}>
+        Get 10% off today:&nbsp;<span>NEW10</span>
+      </Alert>
+
       <Navbar />
+
       <main>{children}</main>
+
       <Footer />
       {displayMinicart && (
         <Suspense fallback={null}>
           <CartSidebar />
         </Suspense>
       )}
+
       {toasts.length > 0 && (
         <Suspense fallback={null}>
           <Toast />
