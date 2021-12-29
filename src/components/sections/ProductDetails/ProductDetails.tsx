7544783b85ad20ceb97cf3f91dd4835f1a13a91b
Filipe W. Lima <filipe.lima@vtex.com.br>
2021-12-29 11:15:44 -0300

    Fix Lighthouse's non-composited animation for the Add to Cart button
    [Lighthouse was complaining about a non-composited animation][1] in the PDP's Add to Cart button.
    After a lot of investigation, we found out the problem was within [the recently added `transition: background-color…`][2] and the very specific scenario of the button starting disabled and being quickly enabled (after the product's data was queried). This is a normal pattern. Except, because this change of state happened very early on the page load, it was captured by Lighthouse, and a background-color transition triggers a re-paint, a non-composite animation ([see the rendering pipeline][3])!
    
    Considering we don't want to remove the CSS `transition: background-color…` because it's valid, and we cannot also change the behaviour of having the button start disabled till it's properly activated, my only solution was to replace the disabled state for [a SVG skeleton component][4], which seemed to fix the non-composited animation violation. Let us confirm when the CI runs the Lighthouse check.
    
    - https://web.dev/non-composited-animations/
    
    [1]: https://github.com/vtex-sites/base.store/runs/4653738587
    [2]: https://github.com/vtex-sites/base.store/commit/509623a44afcfaf6f35a34d04e89d947dc97a89b#diff-2f5b8daef0d1fdc78e04593c1485d310a232ce29ae73ff66dd9ef9d7eb449d0dR11
    [3]: https://web.dev/non-composited-animations/
    [4]: https://skeletonreact.com/

--- src/components/sections/ProductDetails/ProductDetails.tsx
+++ src/components/sections/ProductDetails/ProductDetails.tsx
@@ -77,10 +77,74 @@ function ProductDetails({ product: staleProduct }: Props) {
           { label: 'G', value: 'g' },
         ]}
       />
-      <Button {...buyProps} disabled={isValidating}>
-        Add to cart
-      </Button>
+      {/* NOTE: A loading skeleton had to be used to avoid a Lighthouse's
+      non-composited animation violation due to the button transitioning its
+      background coloe when changing from its initial disabled to active state.
+      See https://web.dev/non-composited-animations/. */}
+      {isValidating ? (
+        <AddToCartLoadingSkeleton />
+      ) : (
+        <Button {...buyProps}>Add to cart</Button>
+      )}
     </div>
+  )
+}
+
+function AddToCartLoadingSkeleton() {
+  // Generated via https://skeletonreact.com/.
+  return (
+    <svg
+      role="img"
+      width="112"
+      height="48"
+      aria-labelledby="loading-aria"
+      viewBox="0 0 112 48"
+      preserveAspectRatio="none"
+    >
+      <title id="loading-aria">Loading...</title>
+      <rect
+        x="0"
+        y="0"
+        width="100%"
+        height="100%"
+        clipPath="url(#clip-path)"
+        style={{ fill: 'url("#fill")' }}
+      />
+      <defs>
+        <clipPath id="clip-path">
+          <rect x="0" y="0" rx="2" ry="2" width="112" height="48" />
+        </clipPath>
+        <linearGradient id="fill">
+          <stop offset="0.599964" stopColor="#f3f3f3" stopOpacity="1">
+            <animate
+              attributeName="offset"
+              values="-2; -2; 1"
+              keyTimes="0; 0.25; 1"
+              dur="2s"
+              repeatCount="indefinite"
+            />
+          </stop>
+          <stop offset="1.59996" stopColor="#ecebeb" stopOpacity="1">
+            <animate
+              attributeName="offset"
+              values="-1; -1; 2"
+              keyTimes="0; 0.25; 1"
+              dur="2s"
+              repeatCount="indefinite"
+            />
+          </stop>
+          <stop offset="2.59996" stopColor="#f3f3f3" stopOpacity="1">
+            <animate
+              attributeName="offset"
+              values="0; 0; 3"
+              keyTimes="0; 0.25; 1"
+              dur="2s"
+              repeatCount="indefinite"
+            />
+          </stop>
+        </linearGradient>
+      </defs>
+    </svg>
   )
 }
 
