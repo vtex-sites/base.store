3fa47535eb003117497d2befa7be5e22c941f465
Lucas Feijó <lucas.portela@vtex.com.br>
2021-12-29 20:56:32 -0300

    Code refactoring

--- src/components/common/Footer/Footer.tsx
+++ src/components/common/Footer/Footer.tsx
@@ -1,13 +1,9 @@
 import React from 'react'
-import { Incentive, List, Icon, PaymentMethods } from '@faststore/ui'
+import { Icon, PaymentMethods } from '@faststore/ui'
 
 import LinksList from './LinksList'
+import Incentives from './Incentives'
 import {
-  QualityIcon,
-  SafetyIcon,
-  GuaranteeIcon,
-  ShippingIcon,
-  StoreIcon,
   FacebookIcon,
   InstagramIcon,
   PinterestIcon,
@@ -27,82 +23,51 @@ function Footer() {
   return (
     <footer>
       {/* Incentives list */}
-      <div>
-        <section>
-          <List variant="unordered">
-            <li>
-              <Incentive>
-                <SafetyIcon />
-                <span>Trusted by SafeCon</span>
-              </Incentive>
-            </li>
-            <li>
-              <Incentive>
-                <QualityIcon />
-                <span>Quality Products</span>
-              </Incentive>
-            </li>
-            <li>
-              <Incentive>
-                <GuaranteeIcon />
-                <span>3-years Guarantee</span>
-              </Incentive>
-            </li>
-            <li>
-              <Incentive>
-                <StoreIcon />
-                <span>Pickup Options</span>
-              </Incentive>
-            </li>
-            <li>
-              <Incentive>
-                <ShippingIcon />
-                <span>Free Shipping</span>
-              </Incentive>
-            </li>
-          </List>
-        </section>
-      </div>
+      <section>
+        <Incentives />
+      </section>
+
       {/* list of links -- must be an accordion on mobile */}
       <LinksList />
+
       {/* Follow us section */}
+
+      <section>
+        <span>Follow us</span>
+        <Icon component={<FacebookIcon />} />
+        <Icon component={<InstagramIcon />} />
+        <Icon component={<PinterestIcon />} />
+        <Icon component={<TwitterIcon />} />
+      </section>
+
+      {/* fast store logo */}
       <div>
-        <section>
-          <span>Follow us</span>
-          <Icon component={<FacebookIcon />} />
-          <Icon component={<InstagramIcon />} />
-          <Icon component={<PinterestIcon />} />
-          <Icon component={<TwitterIcon />} />
-        </section>
+        <Icon component={<FastStoreIcon />} />
       </div>
+
+      {/* small text */}
+      <section>
+        <small>This website uses VTEX technology</small>
+        <small>
+          In-store price may vary. Prices and offers are subject to change. 2021
+          Store name. All rights reserved. Store is a trademark of Store and its
+          affiliated companies.
+        </small>
+        <small> Mount St, 000, New York / NY - 00000.</small>
+      </section>
+
+      {/* payment methods */}
       <div>
-        {/* fast store logo */}
-        <div>
-          <Icon component={<FastStoreIcon />} />
-        </div>
-        {/* small text */}
-        <section>
-          <small>This website uses VTEX technology</small>
-          <small>
-            In-store price may vary. Prices and offers are subject to change.
-            2021 Store name. All rights reserved. Store is a trademark of Store
-            and its affiliated companies.
-          </small>
-          <small> Mount St, 000, New York / NY - 00000.</small>
-        </section>
-        {/* payment methods */}
-        <div>
-          <PaymentMethods title="Payment Methods">
-            <VisaCardIcon />
-            <DinersClubIcon />
-            <PayPalIcon />
-            <MastercardIcon />
-            <StripeIcon />
-            <GooglePayIcon />
-            <EloCardIcon />
-            <ApplePayIcon />
-          </PaymentMethods>
-        </div>
+        <PaymentMethods title="Payment Methods">
+          <VisaCardIcon />
+          <DinersClubIcon />
+          <PayPalIcon />
+          <MastercardIcon />
+          <StripeIcon />
+          <GooglePayIcon />
+          <EloCardIcon />
+          <ApplePayIcon />
+        </PaymentMethods>
       </div>
     </footer>
   )
