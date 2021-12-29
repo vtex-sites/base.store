b50e54c67fd910c089c8e62f624c7b0aa1ab83bf
Israel Costa <israel.costa@vtex.com.br>
2021-12-29 18:59:04 -0300

    feat(ui): Add Breadcrumb (#188)
    * Add Breadcrumb component
    
    * Add aria label to home link
    
    * Parse urls in breadcrumb links

---
+++ src/components/ui/Breadcrumb/Breadcrumb.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { Breadcrumb as UIBreadcrumb } from '@faststore/ui'
+import Link from 'src/components/ui/Link'
+import type { BreadcrumbProps as UIBreadcrumbProps } from '@faststore/ui'
+import { House as HouseIcon } from 'phosphor-react'
+
+type ItemElement = {
+  item: string
+  name: string
+  position: number
+}
+export interface BreadcrumbProps extends UIBreadcrumbProps {
+  breadcrumbList: ItemElement[]
+}
+
+function Breadcrumb({ breadcrumbList, divider }: BreadcrumbProps) {
+  const buildUrl = (url: string) => {
+    const parsedUrl = url.split('/')
+
+    return `/${parsedUrl[parsedUrl.length - 1]}`
+  }
+
+  return (
+    <UIBreadcrumb divider={divider}>
+      <Link aria-label="home" href="/">
+        <HouseIcon />
+      </Link>
+
+      {breadcrumbList.map(({ item, name }, index) => {
+        return breadcrumbList.length === index + 1 ? (
+          <span key={String(index)}>{name}</span>
+        ) : (
+          <Link href={buildUrl(item)} key={String(index)}>
+            {name}
+          </Link>
+        )
+      })}
+    </UIBreadcrumb>
+  )
+}
+
+export default Breadcrumb
