dba19f1bab1ccf8ba3951680fea2c54b42379022
Fanny <fannychienn@gmail.com>
2021-12-29 22:53:15 -0300

    Adds incentives components base

---
+++ src/components/sections/Incentives/Incentives.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import type { ReactNode } from 'react'
+import { Incentive as UIIncentive, List as UIList } from '@faststore/ui'
+
+import './incentives.scss'
+
+interface Incentive {
+  icon: ReactNode
+  title?: string
+  text: string
+}
+
+interface Props {
+  incentives: Incentive[]
+}
+
+function Incentives({ incentives }: Props) {
+  return (
+    <UIList variant="unordered" className="incentives">
+      {incentives.map((incentive, index) => (
+        <li key={String(index)}>
+          <UIIncentive>
+            {incentive.icon}
+            <div className="incentives__item-content">
+              {incentive.title && <p>{incentive.title}</p>}
+              <span>{incentive.text}</span>
+            </div>
+          </UIIncentive>
+        </li>
+      ))}
+    </UIList>
+  )
+}
+
+export default Incentives
