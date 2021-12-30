045161ee6a54382b11b8e5d52039290bc19c8b34
Guilherme Carvalho <guilherme.carvalho@vtex.com.br>
2021-12-30 08:59:15 -0300

    Feat : Style Header (#180)
    * Restructure Navbar header
    
    * Rename file
    
    * Adjusts logo
    
    * Replace logo with simpler component
    
    * Replaces for include media mixin
    
    * Attempts to update selector pattern
    
    * Updates breakpoint
    
    * Fixes plp infinite scroll tests
    
    * Adds svg logo with size
    
    * Use StaticImage from gatsby
    
    * Move NavLinks out of the way
    
    * Style logo img using CSS
    
    * Lighthouse trigger build
    
    * Styles navbar structure
    
    * Adds Navbar's border and box-shadow
    
    * Fixes InputSearch's height on desktop view
    
    * Adds Alert to Layout
    
    * Adds styled Links
    
    * Fixes navbar wrapper
    
    * Use localStorage to handle alert close/open state
    
    * Scope query for store-input and store-button on search test
    
    * Fixes CartToggle sizing
    
    * Styles header on mobile and desktop views
    
    Also brings Navlink to Navbar file
    
    * Changes Aside to regular div
    
    * Removes navlinks
    
    * Attempts to fix icon colors
    
    * Close menu when navigating to homepage
    
    * Fix SonarQube reply
    
    * Removes visited color from navbar links
    
    * Fixes search integration test
    
    Co-authored-by: Fanny <fannychienn@gmail.com>
    Co-authored-by: eduardoformiga <eduardo.formiga@vtex.com.br>
    Co-authored-by: renatamottam <renatamotta.m@gmail.com>

--- src/components/common/Navbar/Navbar.tsx
+++ src/components/common/Navbar/Navbar.tsx
@@ -1,15 +1,83 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link as LinkGatsby } from 'gatsby'
 import CartToggle from 'src/components/cart/CartToggle'
+import Logo from 'src/components/ui/Logo'
+import Link from 'src/components/ui/Link'
+import Button from 'src/components/ui/Button'
+import { List as ListIcon, X as XIcon } from 'phosphor-react'
 
-import Navlinks from '../Navlinks'
 import SearchInput from '../SearchInput'
 
+import './navbar.scss'
+
+const links = [
+  {
+    href: '/apparel-and-accesories',
+    name: 'Apparel',
+  },
+  {
+    href: '/office',
+    name: 'Office',
+  },
+]
+
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false)
+
   return (
-    <header>
-      <Navlinks />
-      <SearchInput />
-      <CartToggle />
+    <header className="navbar grid-content-full">
+      <div className="navbar__header grid-content">
+        <section className="navbar__row">
+          <Button
+            className="navbar__menu"
+            aria-label="Open Menu"
+            onClick={() => setShowMenu(true)}
+          >
+            <ListIcon size={32} />
+          </Button>
+          <LinkGatsby
+            to="/"
+            aria-label="Go to Faststore home"
+            title="Go to Faststore home"
+            className="navbar__logo"
+            onClick={() => setShowMenu(false)}
+          >
+            <Logo />
+          </LinkGatsby>
+          <div className="navbar__buttons">
+            <CartToggle />
+          </div>
+          <SearchInput />
+        </section>
+
+        <div className={`navlinks ${showMenu ? 'open' : ''}`}>
+          <section>
+            <LinkGatsby
+              to="/"
+              aria-label="Go to Faststore home"
+              title="Go to Faststore home"
+              className="navbar__logo"
+              onClick={() => setShowMenu(false)}
+            >
+              <Logo />
+            </LinkGatsby>
+            <Button
+              className="navlinks__button"
+              aria-label="Close Menu"
+              onClick={() => setShowMenu(false)}
+            >
+              <XIcon size={24} weight="bold" />
+            </Button>
+          </section>
+          <nav className="navlinks__list">
+            {links.map((x) => (
+              <Link variant="display" key={x.href} href={x.href}>
+                {x.name}
+              </Link>
+            ))}
+          </nav>
+        </div>
+      </div>
     </header>
   )
 }
