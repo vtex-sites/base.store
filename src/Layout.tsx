97eea9c15e55a3da5f35f55b42d9e2ffedce8733
renatamottam <renatamotta.m@gmail.com>
2022-01-03 11:54:28 -0300

    Attempts to improves font loading

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -37,6 +37,10 @@ function Layout({ children }: PropsWithChildren<unknown>) {
     <>
       <Helmet>
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700;900&display=swap"
+          rel="stylesheet"
+        />
       </Helmet>
 
       {showAlert && (
