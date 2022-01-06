84c74ee01b680ea5cbab97b02f9cd029af31e679
eduardoformiga <eduardo.formiga@vtex.com.br>
2022-01-06 20:34:29 -0300

    Removes temp-offscreen classes

--- src/pages/{StoreCollection.slug}.tsx
+++ src/pages/{StoreCollection.slug}.tsx
@@ -86,9 +86,7 @@ function Page(props: Props) {
         Sections: Components imported from '../components/sections' only.
         Do not import or render components from any other folder in here.
       */}
-      <h1 data-testid="collection-page" className="temp-offscreen">
-        {title}
-      </h1>
+      <h1 data-testid="collection-page">{title}</h1>
 
       <ProductGallery title={title} />
     </SearchProvider>
