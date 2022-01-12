6d9bf3142c37aae1d3036daffa9567a54243a5d7
eduardoformiga <eduardo.formiga@vtex.com.br>
2022-01-12 19:40:22 -0300

    Keeps minimum acceptable css

--- src/components/product/ProductGrid/ProductGrid.tsx
+++ src/components/product/ProductGrid/ProductGrid.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import type { ProductSummary_ProductFragment } from '@generated/graphql'
 
 import ProductCard from '../ProductCard'
+import './product-grid.scss'
 
 interface Props {
   products: ProductSummary_ProductFragment[]
@@ -11,7 +12,7 @@ interface Props {
 
 function ProductGrid({ products, page, pageSize }: Props) {
   return (
-    <ul>
+    <ul className="product-grid">
       {products.map((product, idx) => (
         <li key={`${product.id}`}>
           <ProductCard product={product} index={pageSize * page + idx + 1} />
