vtex-sites/base.store

commit 97c2d399fe8d3bba31ad264daecc53a4604d851b

Style the gray results area

src/components/sections/ProductGallery/ProductGallery.tsx
@@ -24,70 +24,74 @@ function ProductGallery({ title }: Props) {
 
   return (
     <>
-      {/* Controls */}
-      <FacetedFilter facets={data.search.facets} />
-      <div>
-        <div data-testid="total-product-count" data-count={totalCount}>
-          Total Products: {totalCount}
-        </div>
-        <h2>Most Wanted</h2>
+      <div
+        className="plp-results-count"
+        data-testid="total-product-count"
+        data-count={totalCount}
+      >
+        {totalCount} Results
       </div>
 
-      {/* Add link to previous page. This helps on SEO */}
-      {prev !== false && (
-        <>
-          <GatsbySeo linkTags={[{ rel: 'prev', href: prev.link }]} />
-          <a
-            onClick={(e) => {
-              e.currentTarget.blur()
-              e.preventDefault()
-              addPrevPage()
-            }}
-            href={prev.link}
-            rel="prev"
-          >
-            Previous Page
-          </a>
-        </>
-      )}
+      <div className="plp-results-results">
+        {/* Controls */}
+        <FacetedFilter facets={data.search.facets} />
 
-      {/* Render ALL products */}
-      {pages.map((page) => (
-        <GalleryPage
-          key={`gallery-page-${page}`}
-          fallbackData={page === searchState.page ? data : undefined}
-          page={page}
-          title={title}
-        />
-      ))}
+        {/* Add link to previous page. This helps on SEO */}
+        {prev !== false && (
+          <>
+            <GatsbySeo linkTags={[{ rel: 'prev', href: prev.link }]} />
+            <a
+              onClick={(e) => {
+                e.currentTarget.blur()
+                e.preventDefault()
+                addPrevPage()
+              }}
+              href={prev.link}
+              rel="prev"
+            >
+              Previous Page
+            </a>
+          </>
+        )}
 
-      {/* Add link to next page. This helps on SEO */}
-      {next !== false && (
-        <>
-          <GatsbySeo linkTags={[{ rel: 'next', href: next.link }]} />
-          <LinkButton
-            data-testid="show-more"
-            onClick={(e) => {
-              e.currentTarget.blur()
-              e.preventDefault()
-              addNextPage()
-            }}
-            href={next.link}
-            rel="next"
-            variant="secondary"
-          >
-            Load more products
-          </LinkButton>
-        </>
-      )}
+        {/* Render ALL products */}
+        {pages.map((page) => (
+          <GalleryPage
+            key={`gallery-page-${page}`}
+            fallbackData={page === searchState.page ? data : undefined}
+            page={page}
+            title={title}
+          />
+        ))}
 
-      {/* Prefetch Previous and Next pages */}
-      {prev !== false && (
-        <GalleryPage page={prev.cursor} display={false} title={title} />
-      )}
-      {next !== false && (
-        <GalleryPage page={next.cursor} display={false} title={title} />
-      )}
+        {/* Add link to next page. This helps on SEO */}
+        {next !== false && (
+          <>
+            <GatsbySeo linkTags={[{ rel: 'next', href: next.link }]} />
+            <LinkButton
+              data-testid="show-more"
+              onClick={(e) => {
+                e.currentTarget.blur()
+                e.preventDefault()
+                addNextPage()
+              }}
+              href={next.link}
+              rel="next"
+              variant="secondary"
+            >
+              Load more products
+            </LinkButton>
+          </>
+        )}
+
+        {/* Prefetch Previous and Next pages */}
+        {prev !== false && (
+          <GalleryPage page={prev.cursor} display={false} title={title} />
+        )}
+        {next !== false && (
+          <GalleryPage page={next.cursor} display={false} title={title} />
+        )}
+      </div>
     </>
   )
 }

src/pages/{StoreCollection.slug}.tsx
@@ -110,7 +110,9 @@ function Page(props: Props) {
         <button>Filters</button>
       </div>
 
-      <ProductGallery title={title} />
+      <div className="plp-results-wrapper">
+        <ProductGallery title={title} />
+      </div>
     </SearchProvider>
   )
 }

src/styles/pages/plp.scss
@@ -23,3 +23,22 @@
   min-height: 68px;
   padding: var(--space-1) var(--space-3) var(--space-2);
 }
+
+.plp-results-wrapper {
+  width: 100%;
+  background-color: var(--color-neutral-1);
+}
+
+.plp-results-count {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 45px;
+  padding: var(--space-2) var(--space-3);
+}
+
+.plp-results-results {
+  --padding: var(--space-1);
+
+  padding: var(--padding);
+}

commit 42a610737b745ad9dd93f08691b501b6b60ca0b3

Fix an a11y issue of out-of-order headings
Introduced in https://github.com/vtex-sites/base.store/commit/97c2d399fe8d3bba31ad264daecc53a4604d851b when I removed the `h2` so there was a jump from `h1` to product cards' `h3`.

src/components/sections/ProductGallery/ProductGallery.tsx
@@ -3,6 +3,7 @@ import { GatsbySeo } from 'gatsby-plugin-next-seo'
 import React from 'react'
 import FacetedFilter from 'src/components/search/FacetedFilter'
 import { LinkButton } from 'src/components/ui/Button'
+import SROnly from 'src/components/ui/SROnly'
 
 import GalleryPage from './ProductGalleryPage'
 import { useGalleryQuery } from './useGalleryQuery'
@@ -29,6 +30,9 @@ function ProductGallery({ title }: Props) {
         data-testid="total-product-count"
         data-count={totalCount}
       >
+        <h2>
+          <SROnly text="Search results" />
+        </h2>
         {totalCount} Results
       </div>
 