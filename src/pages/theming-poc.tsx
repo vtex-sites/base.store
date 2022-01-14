5db0bb76516d378b3f979b2c0479b499a0453da5
Sérgio Fontes <sergio@fontes.cc>
2022-01-14 19:39:43 -0300

    Style a toggle component alongside new design tokens to customize it

--- src/pages/theming-poc.tsx
+++ src/pages/theming-poc.tsx
@@ -1,18 +1,62 @@
 import React from 'react'
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
 
+import '../styles/theming-poc.scss'
+
 function Page() {
   return (
     <>
-      <GatsbySeo title="Pattern Library" language="en" noindex nofollow />
-      <div className="pattern-library">
-        <header>
-          <h1 className="title-section grid-content">
-            UI Theming Proof of Concept
-          </h1>
-        </header>
+      <GatsbySeo
+        title="UI Theming: Proof of Concept"
+        language="en"
+        noindex
+        nofollow
+      />
+      <div className="theming-poc">
+        <header />
+        <main>
+          <InputToggle />
+          <InputToggle2 />
+          <InputToggle3 />
+        </main>
       </div>
     </>
+  )
+}
+
+function InputToggle() {
+  return (
+    <div className="input-toggle">
+      <label htmlFor="input-toggle">How about this?</label>
+      <div className="container">
+        <input id="input-toggle" name="input-toggle" type="checkbox" />
+        <span className="knob" />
+      </div>
+    </div>
+  )
+}
+
+function InputToggle2() {
+  return (
+    <div className="input-toggle theme-2">
+      <label htmlFor="input-toggle">How about this?</label>
+      <div className="container">
+        <input id="input-toggle-1" name="input-toggle" type="checkbox" />
+        <span className="knob" />
+      </div>
+    </div>
+  )
+}
+
+function InputToggle3() {
+  return (
+    <div className="input-toggle theme-3">
+      <label htmlFor="input-toggle">How about this?</label>
+      <div className="container">
+        <input id="input-toggle-2" name="input-toggle" type="checkbox" />
+        <span className="knob" />
+      </div>
+    </div>
   )
 }
 
