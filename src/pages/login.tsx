b960af1834d12fe8a8d23d5efc762e898107a2fa
Larícia Mota <laricia.mota@vtex.com.br>
2022-01-17 15:50:09 -0300

    Test: use createRedirect

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,15 +1,7 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
 
-import * as storeConfig from '../../store.config'
-
-const { loginUrl } = storeConfig
-
 function Page() {
-  useEffect(() => {
-    window.location.href = loginUrl
-  }, [])
-
   return <GatsbySeo noindex nofollow />
 }
 
