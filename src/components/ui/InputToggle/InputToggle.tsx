f1d4684a279382db6ba17b7bc4bd6e3056b0b5bb
Sérgio Fontes <sergio@fontes.cc>
2022-01-18 12:15:37 -0300

    Update data attributes in InputToggle

--- src/components/ui/InputToggle/InputToggle.tsx
+++ src/components/ui/InputToggle/InputToggle.tsx
@@ -4,11 +4,11 @@ import './input-toggle.scss'
 
 function InputToggle() {
   return (
-    <div data-toggle>
+    <div data-base-toggle>
       <label htmlFor="input-toggle">How about this?</label>
-      <div data-toggle-container>
+      <div data-base-toggle-container>
         <input id="" name="input-toggle" type="checkbox" />
-        <span data-toggle-knob />
+        <span data-base-toggle-knob />
       </div>
     </div>
   )
