30c6e4de33f0528257ecf50fec203c0a88869dd3
Filipe W. Lima <filipe.lima@vtex.com.br>
2022-01-18 15:44:23 -0300

    Style the recently integrated Sponsored Products
    There's still a bug in the product tiles for the desktop version. There's some weird gap between the image and the product name. We should investigate later.

--- src/components/sections/ProductGallery/ProductGalleryPage.tsx
+++ src/components/sections/ProductGallery/ProductGalleryPage.tsx
@@ -97,10 +97,10 @@ function GalleryPage({
             page={page}
             pageSize={middleItemIndex}
           />
-          <div>
+          <div className="plp-results-sponsored">
             <h3>Sponsored</h3>
+            <ProductTiles products={productsSponsored.slice(0, 2)} />
           </div>
-          <ProductTiles products={productsSponsored.slice(0, 2)} />
           <ProductGrid
             products={products.slice(middleItemIndex, itemsPerPage)}
             page={page}
