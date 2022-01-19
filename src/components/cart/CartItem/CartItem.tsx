8890c99974f2cf982935fdfb70f50a80449c00ee
Daniel Zanzini <daniel.zanzini@vtex.com.br>
2022-01-19 12:31:58 -0300

    Adds style to MiniCartItem content

--- src/components/cart/CartItem/CartItem.tsx
+++ src/components/cart/CartItem/CartItem.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import Button from 'src/components/ui/Button'
 import { Image } from 'src/components/ui/Image'
+import Price from 'src/components/ui/Price'
 import type { CartItemWithAnalytics } from 'src/sdk/cart/useBuyButton'
 import { useRemoveButton } from 'src/sdk/cart/useRemoveButton'
 import { useFormattedPrice } from 'src/sdk/product/useFormattedPrice'
+
+import './cart-item.scss'
 
 interface Props {
   item: CartItemWithAnalytics
@@ -11,34 +14,58 @@ interface Props {
 
 function CartItem({ item }: Props) {
   const btnProps = useRemoveButton(item)
-  const price = useFormattedPrice(item.price)
-  const listPrice = useFormattedPrice(item.listPrice)
 
   return (
     <div
+      className="cart-item"
+      data-cart-item
       data-testid="cart-item"
       data-sku={item.itemOffered.sku}
       data-seller={item.seller.identifier}
     >
-      <Image
-        baseUrl={item.itemOffered.image[0].url}
-        alt={item.itemOffered.image[0].alternateName}
-        sourceWidth={720}
-        aspectRatio={1}
-        width={100}
-        breakpoints={[50, 100, 150]}
-        layout="constrained"
-        backgroundColor="#f0f0f0"
-        options={{
-          fitIn: true,
-        }}
-      />
-      <div>name: {item.itemOffered.name}</div>
+      <div data-cart-item-content>
+        <Image
+          baseUrl={item.itemOffered.image[0].url}
+          alt={item.itemOffered.image[0].alternateName}
+          sourceWidth={360}
+          aspectRatio={1}
+          width={60}
+          breakpoints={[50, 100, 150]}
+          layout="constrained"
+          backgroundColor="#f0f0f0"
+          options={{
+            fitIn: true,
+          }}
+        />
+        <div data-cart-item-summary>
+          <div className="text-body">{item.isVariantOf?.name}</div>
+          <div data-cart-item-price>
+            <Price
+              value={item.listPrice}
+              formatter={useFormattedPrice}
+              testId="list-price"
+              data-value={item.listPrice}
+              variant="listing"
+              classes="text-body-small"
+              SRText="Original price:"
+            />
+            <Price
+              value={item.price}
+              formatter={useFormattedPrice}
+              testId="price"
+              data-value={item.price}
+              variant="spot"
+              classes="title-subsection"
+              SRText="Price:"
+            />
+          </div>
+        </div>
+      </div>
+
       <div>sku: {item.itemOffered.sku}</div>
       <div>id: {item.id}</div>
       <div>seller: {item.seller.identifier}</div>
-      <div>price: {price}</div>
-      <div>listPrice: {listPrice}</div>
+
       <div>quantity: {item.quantity}</div>
       <Button {...btnProps}>Remove Item</Button>
     </div>
