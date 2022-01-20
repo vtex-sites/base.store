842982652856656787976b47354171aee0e59a17
Sara Lins <saranicoly0@gmail.com>
2022-01-20 10:22:49 -0300

    organize EmptyCart on its own folder

---
+++ src/components/cart/EmptyCart/EmptyCart.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from 'src/components/ui/Button'
+import { ShoppingCart as ShoppingCartIcon } from 'phosphor-react'
+import { useCartToggleButton } from 'src/sdk/cart/useCartToggleButton'
+
+function EmptyCart() {
+  const toggleProps = useCartToggleButton()
+
+  return (
+    <div data-testid="cart-empty-state">
+      <ShoppingCartIcon size="32" />
+      <span>Your Cart is empty</span>
+      <Button {...toggleProps}>Start Shopping</Button>
+    </div>
+  )
+}
+
+export default EmptyCart
