08bbe242e0de95a3a45724ad29af8d4ab101b907
Sara Lins <saranicoly0@gmail.com>
2022-01-20 10:00:14 -0300

    add closeButton named as start shopping

--- src/components/cart/CartSidebar/CartSidebar.tsx
+++ src/components/cart/CartSidebar/CartSidebar.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Button from 'src/components/ui/Button'
 import { ShoppingCart as ShoppingCartIcon } from 'phosphor-react'
+import { useCartToggleButton } from 'src/sdk/cart/useCartToggleButton'
 
 function CartSidebar() {
+  const toggleProps = useCartToggleButton()
+
   return (
-    <div data-testid="cart-sidebar">
-      <ShoppingCartIcon size="25" weight="bold" />
-      <div>Your cart is empty</div>
-      <Button>Start Shopping</Button>
+    <div data-testid="cart-empty-state">
+      <ShoppingCartIcon size="32" />
+      <span>Your Cart is empty</span>
+      <Button {...toggleProps}>Start Shopping</Button>
     </div>
   )
 }
