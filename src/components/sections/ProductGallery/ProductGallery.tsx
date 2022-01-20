c8424df57e312d4cd665ac04b907d309f8b9e2b8
Arthur Andrade <arthurfelandrade@gmail.com>
2022-01-20 18:40:23 -0300

    fix: loading filters inplp
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- src/components/sections/ProductGallery/ProductGallery.tsx
+++ src/components/sections/ProductGallery/ProductGallery.tsx
@@ -1,11 +1,12 @@
 import { usePagination, useSearch } from '@faststore/sdk'
 import { GatsbySeo } from 'gatsby-plugin-next-seo'
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo, useRef } from 'react'
 import Button, { LinkButton } from 'src/components/ui/Button'
 import useWindowDimensions from 'src/hooks/useWindowDimensions'
 import Filter from 'src/components/search/Filter'
 import Sort from 'src/components/search/Sort'
 import { FadersHorizontal as FadersHorizontalIcon } from 'phosphor-react'
+import type { FacetedFilter_FacetsFragment } from '@generated/graphql'
 
 import GalleryPage from './ProductGalleryPage'
 import { useGalleryQuery } from './useGalleryQuery'
@@ -17,6 +18,7 @@ interface Props {
 function ProductGallery({ title }: Props) {
   const { pages, state: searchState, addNextPage, addPrevPage } = useSearch()
   const { data } = useGalleryQuery()
+  const facets = useRef<FacetedFilter_FacetsFragment[]>([])
 
   const totalCount = data?.search.products.pageInfo.totalCount ?? 0
   const { next, prev } = usePagination(totalCount)
@@ -38,7 +40,25 @@ function ProductGallery({ title }: Props) {
     setIsMobile(screenWidth < parseInt(notebookBreakpoint, 10))
   }, [screenWidth])
 
-  if (!data) {
+  const orderedFacets: FacetedFilter_FacetsFragment[] = useMemo(() => {
+    if (data) {
+      const orderFacets = data.search.facets.map((facet) => {
+        if (facet.type === 'BOOLEAN') {
+          facet.values = facet.values.sort((a, b) =>
+            a.label.localeCompare(b.label)
+          )
+        }
+
+        return facet
+      })
+
+      facets.current = orderFacets
+    }
+
+    return facets.current
+  }, [data])
+
+  if (!facets.current.length) {
     return <div className="plp-temp-data-loading">loading...</div>
   }
 
@@ -48,93 +68,99 @@ function ProductGallery({ title }: Props) {
         <div className="plp-filters-wrapper">
           <Filter
             isOpen={isFilterOpen}
-            facets={data.search.facets}
+            facets={orderedFacets}
             onDismiss={() => setIsFilterOpen(false)}
           />
         </div>
 
-        <div
-          className="plp-results-count-wrapper"
-          data-testid="total-product-count"
-          data-count={totalCount}
-        >
-          <h2>{totalCount} Results</h2>
-        </div>
-
-        <div className="plp-sort-wrapper">
-          <Sort />
-
-          {isMobile && (
-            <Button
-              variant="tertiary"
-              data-testid="open-filter-button"
-              icon={<FadersHorizontalIcon size={16} />}
-              iconPosition="left"
-              aria-label="Open Filters"
-              onClick={() => setIsFilterOpen(!isFilterOpen)}
+        {data ? (
+          <>
+            <div
+              className="plp-results-count-wrapper"
+              data-testid="total-product-count"
+              data-count={totalCount}
             >
-              Filters
-            </Button>
-          )}
-        </div>
-
-        <div className="plp-results-wrapper">
-          {/* Add link to previous page. This helps on SEO */}
-          {prev !== false && (
-            <>
-              <GatsbySeo linkTags={[{ rel: 'prev', href: prev.link }]} />
-              <a
-                onClick={(e) => {
-                  e.currentTarget.blur()
-                  e.preventDefault()
-                  addPrevPage()
-                }}
-                href={prev.link}
-                rel="prev"
-              >
-                Previous Page
-              </a>
-            </>
-          )}
-
-          {/* Render ALL products */}
-          {pages.map((page) => (
-            <GalleryPage
-              key={`gallery-page-${page}`}
-              fallbackData={page === searchState.page ? data : undefined}
-              page={page}
-              title={title}
-            />
-          ))}
-
-          {/* Prefetch Previous and Next pages */}
-          {prev !== false && (
-            <GalleryPage page={prev.cursor} display={false} title={title} />
-          )}
-          {next !== false && (
-            <GalleryPage page={next.cursor} display={false} title={title} />
-          )}
-
-          {/* Add link to next page. This helps on SEO */}
-          {next !== false && (
-            <div className="plp-load-more-wrapper">
-              <GatsbySeo linkTags={[{ rel: 'next', href: next.link }]} />
-              <LinkButton
-                data-testid="show-more"
-                onClick={(e) => {
-                  e.currentTarget.blur()
-                  e.preventDefault()
-                  addNextPage()
-                }}
-                href={next.link}
-                rel="next"
-                variant="secondary"
-              >
-                Load more products
-              </LinkButton>
+              <h2>{totalCount} Results</h2>
             </div>
-          )}
-        </div>
+
+            <div className="plp-sort-wrapper">
+              <Sort />
+
+              {isMobile && (
+                <Button
+                  variant="tertiary"
+                  data-testid="open-filter-button"
+                  icon={<FadersHorizontalIcon size={16} />}
+                  iconPosition="left"
+                  aria-label="Open Filters"
+                  onClick={() => setIsFilterOpen(!isFilterOpen)}
+                >
+                  Filters
+                </Button>
+              )}
+            </div>
+
+            <div className="plp-results-wrapper">
+              {/* Add link to previous page. This helps on SEO */}
+              {prev !== false && (
+                <>
+                  <GatsbySeo linkTags={[{ rel: 'prev', href: prev.link }]} />
+                  <a
+                    onClick={(e) => {
+                      e.currentTarget.blur()
+                      e.preventDefault()
+                      addPrevPage()
+                    }}
+                    href={prev.link}
+                    rel="prev"
+                  >
+                    Previous Page
+                  </a>
+                </>
+              )}
+
+              {/* Render ALL products */}
+              {pages.map((page) => (
+                <GalleryPage
+                  key={`gallery-page-${page}`}
+                  fallbackData={page === searchState.page ? data : undefined}
+                  page={page}
+                  title={title}
+                />
+              ))}
+
+              {/* Prefetch Previous and Next pages */}
+              {prev !== false && (
+                <GalleryPage page={prev.cursor} display={false} title={title} />
+              )}
+              {next !== false && (
+                <GalleryPage page={next.cursor} display={false} title={title} />
+              )}
+
+              {/* Add link to next page. This helps on SEO */}
+              {next !== false && (
+                <div className="plp-load-more-wrapper">
+                  <GatsbySeo linkTags={[{ rel: 'next', href: next.link }]} />
+                  <LinkButton
+                    data-testid="show-more"
+                    onClick={(e) => {
+                      e.currentTarget.blur()
+                      e.preventDefault()
+                      addNextPage()
+                    }}
+                    href={next.link}
+                    rel="next"
+                    variant="secondary"
+                  >
+                    Load more products
+                  </LinkButton>
+                </div>
+              )}
+            </div>
+          </>
+        ) : (
+          <div className="plp-temp-data-loading">loading...</div>
+        )}
       </div>
     </div>
   )
