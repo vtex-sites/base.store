9d00e9715102f2c86c69039b29d3adb5ca43fce8
eduardoformiga <eduardo.formiga@vtex.com.br>
2022-01-19 21:20:36 -0300

    Testing critical css (Above the fold) on homepage only

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,5 +1,6 @@
 const path = require('path')
 
+const critical = require('critical')
 const fs = require('fs-extra')
 
 exports.onPreInit = ({ reporter }) => {
@@ -14,12 +15,12 @@ exports.onPreInit = ({ reporter }) => {
   )
 }
 
-exports.onCreateWebpackConfig = ({ actions: { setWebpackConfig }, stage }) => {
+exports.onCreateWebpackConfig = ({ actions, stage }) => {
   const profiling = process.env.GATSBY_STORE_PROFILING === 'true'
 
   if (stage === 'build-javascript') {
     if (profiling) {
-      setWebpackConfig({
+      actions.setWebpackConfig({
         optimization: {
           minimize: false,
           moduleIds: 'named',
@@ -28,7 +29,7 @@ exports.onCreateWebpackConfig = ({ actions: { setWebpackConfig }, stage }) => {
         },
       })
     } else {
-      setWebpackConfig({
+      actions.setWebpackConfig({
         optimization: {
           runtimeChunk: {
             name: `webpack-runtime`,
@@ -54,5 +55,53 @@ exports.onCreateBabelConfig = ({ actions }) => {
   actions.setBabelPlugin({
     name: `@vtex/graphql-utils/babel`,
     options: {},
+  })
+}
+
+exports.onPostBuild = ({ reporter }) => {
+  reporter.info(`Your Gatsby site has been built!`)
+
+  critical.generate({
+    // Inline the generated critical-path CSS
+    // - true generates HTML
+    // - false generates CSS
+    inline: true,
+
+    // Your base directory
+    base: 'public/',
+
+    // HTML source file
+    src: 'index.html',
+    // dest: 'index.html',
+
+    // Your CSS Files (optional)
+    // css: ['dist/styles/main.css'],
+
+    dimensions: [
+      {
+        width: 500,
+        height: 900,
+      },
+      {
+        width: 1280,
+        height: 900,
+      },
+    ],
+
+    // Output results to file
+    target: {
+      css: 'critical.css',
+      // html: 'index-critical.html',
+      html: 'index.html',
+      uncritical: 'uncritical.css',
+    },
+
+    // Extract inlined styles from referenced stylesheets
+    extract: true,
+
+    // ignore CSS rules
+    ignore: {
+      atrule: ['@font-face'],
+    },
   })
 }
