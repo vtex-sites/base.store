8b08fd343e892e7668b3a7ed396262dc370ad4b0
eduardoformiga <eduardo.formiga@vtex.com.br>
2022-01-20 13:48:32 -0300

    Tests purgeCss approach

--- gatsby-config.js
+++ gatsby-config.js
@@ -180,5 +180,21 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-postcss',
     },
+    // Add after these plugins if used
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // ignore: ['.cache/*.css'], // Ignore files/folders
+        purgeOnly: ['src/styles/global.scss'], // Purge only these files/folders
+        purgeCSSOptions: {
+          // https://purgecss.com/configuration.html#options
+          safelist: [/data-.*/], // Don't remove this selector
+        },
+        // More options defined here https://purgecss.com/configuration.html#options
+      },
+    },
   ],
 }
