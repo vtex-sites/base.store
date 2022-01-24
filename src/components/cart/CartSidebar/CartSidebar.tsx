049aa187f164bb6ae4350b1227f7ce2daf3f6df8
Sara Lins <saranicoly0@gmail.com>
2022-01-24 19:38:19 -0300

    implements slideOver

--- src/components/cart/CartSidebar/CartSidebar.tsx
+++ src/components/cart/CartSidebar/CartSidebar.tsx
@@ -10,6 +10,8 @@ import {
 } from 'phosphor-react'
 import { Badge } from 'src/components/ui/Badge'
 import Alert from 'src/components/ui/Alert'
+import SlideOver from 'src/components/ui/SlideOver'
+import { useUI } from 'src/sdk/ui'
 
 import CartItem from '../CartItem'
 import OrderSummary from '../OrderSummary'
@@ -21,58 +23,70 @@ function CartSidebar() {
   const btnProps = useCheckoutButton()
   const cart = useCart()
   const toggleProps = useCartToggleButton()
+  const { displayMinicart, openMinicart, closeMinicart } = useUI()
+
   const { items, gifts, totalItems, isValidating, subTotal, total } = cart
 
+  let onDismissTransition: () => unknown
+
   return (
-    <div cart-sidebar data-testid="cart-sidebar">
-      <div cart-sidebar-header>
-        <p className="title-section">Your Cart</p>
-        <Badge variant="new" small>
-          {totalItems}
-        </Badge>
-        <Button
-          variant="tertiary"
-          icon={<XIcon size={18} weight="bold" />}
-          iconPosition="right"
-          {...toggleProps}
-        />
-        <Alert icon={<TruckIcon size={24} />}>
-          Free shiping starts at $300
-        </Alert>
-      </div>
-      {totalItems > 0 ? (
-        <>
-          {items.map((item) => (
-            <CartItem key={item.id} item={item} />
-          ))}
-
-          {/* TODO: add conditional here */}
-          <div>Gifts</div>
-          {gifts.map((item) => (
-            <CartItem key={item.id} item={item} />
-          ))}
-
-          <OrderSummary
-            subTotal={subTotal}
-            total={total}
-            numberOfItems={totalItems}
-            checkoutButton={
-              <Button
-                data-cart-checkout-button
-                variant="primary"
-                icon={!isValidating && <ArrowRightIcon size={18} />}
-                iconPosition="right"
-                {...btnProps}
-              >
-                {isValidating ? 'Loading...' : 'Checkout'}
-              </Button>
-            }
+    <SlideOver
+      isOpen={displayMinicart}
+      onDismiss={closeMinicart}
+      onDismissTransition={(callback) => (onDismissTransition = callback)}
+      size="partial"
+      direction="rightSide"
+    >
+      <div cart-sidebar data-testid="cart-sidebar">
+        <div cart-sidebar-header>
+          <p className="title-section">Your Cart</p>
+          <Badge variant="new" small>
+            {totalItems}
+          </Badge>
+          <Button
+            variant="tertiary"
+            icon={<XIcon size={18} weight="bold" />}
+            iconPosition="right"
+            {...toggleProps}
           />
-        </>
-      ) : (
-        <EmptyCart />
-      )}
-    </div>
+          <Alert icon={<TruckIcon size={24} />}>
+            Free shiping starts at $300
+          </Alert>
+        </div>
+        {totalItems > 0 ? (
+          <>
+            {items.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
+
+            {/* TODO: add conditional here */}
+            <div>Gifts</div>
+            {gifts.map((item) => (
+              <CartItem key={item.id} item={item} />
+            ))}
+
+            <OrderSummary
+              subTotal={subTotal}
+              total={total}
+              numberOfItems={totalItems}
+              checkoutButton={
+                <Button
+                  data-cart-checkout-button
+                  variant="primary"
+                  icon={!isValidating && <ArrowRightIcon size={18} />}
+                  iconPosition="right"
+                  {...btnProps}
+                >
+                  {isValidating ? 'Loading...' : 'Checkout'}
+                </Button>
+              }
+            />
+          </>
+        ) : (
+          <EmptyCart />
+        )}
+      </div>
+    </SlideOver>
   )
 }
 
