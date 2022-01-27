2fe9f3ab76792fc6eaaec00db23b8b8c5acfd734
Fanny <fannychienn@gmail.com>
2022-01-27 13:40:52 -0300

    Adds onDismiss function for Empty Cart

--- src/components/cart/EmptyCart/EmptyCart.tsx
+++ src/components/cart/EmptyCart/EmptyCart.tsx
@@ -1,20 +1,24 @@
 import React from 'react'
 import Button from 'src/components/ui/Button'
 import { ShoppingCart as ShoppingCartIcon } from 'phosphor-react'
-import { useCartToggleButton } from 'src/sdk/cart/useCartToggleButton'
 
 import './empty-cart.scss'
 
-function EmptyCart() {
-  const toggleProps = useCartToggleButton()
+interface Props {
+  /**
+   * This function is called when `Start Shopping` button is clicked
+   */
+  onDismiss: () => void
+}
 
+function EmptyCart({ onDismiss }: Props) {
   return (
     <div data-testid="cart-empty-state" data-empty-cart>
       <div data-empty-cart-title>
         <ShoppingCartIcon size="32" />
         <p>Your Cart is empty</p>
       </div>
-      <Button {...toggleProps} variant="primary">
+      <Button onClick={onDismiss} variant="primary">
         Start Shopping
       </Button>
     </div>
