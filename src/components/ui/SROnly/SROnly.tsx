96ea67b079f9ec2da962b4f709ba0dea25a5cf98
Eduardo Formiga <eduardo.formiga@gmail.com>
2022-02-03 13:30:19 -0300

    [FSSS-170] Chore: tweaks search page (#293)
    * Removes unused title
    
    * Extracts css from product-gallery section
    
    * Tweaks SROnly component
    
    * Adds SROnly h1 title to search page
    
    * Fixes typo

--- src/components/ui/SROnly/SROnly.tsx
+++ src/components/ui/SROnly/SROnly.tsx
@@ -1,13 +1,17 @@
+import type { ElementType } from 'react'
 import React from 'react'
 
 import './sr-only.scss'
 
 interface Props {
   text: string
+  as?: ElementType
 }
 
-function SROnly({ text }: Props) {
-  return <span data-store-sr-only>{text}</span>
+function SROnly({ text, as }: Props) {
+  const Component = as ?? 'span'
+
+  return <Component data-store-sr-only>{text}</Component>
 }
 
 export default SROnly
