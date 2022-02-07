0365b89a3bb3f2ac41711e4edc063fdc8a25d372
Ícaro Azevedo <icazevedo10@gmail.com>
2022-02-07 18:26:11 -0300

    chore: adds source to comment about cleaning ecommerce object

--- src/sdk/analytics/index.tsx
+++ src/sdk/analytics/index.tsx
@@ -35,6 +35,8 @@ export const AnalyticsHandler = () => {
     // Cleans the ecommerce object before pushing a new one
     // This prevents the new data from getting perged with the previous one
     // which could lead do inacurate and old data being sent with events
+    //
+    // source: https://developers.google.com/tag-manager/ecommerce-ga4?hl=pt-br#clearing_the_ecommerce_object
     window.dataLayer.push({ ecommerce: null })
     window.dataLayer.push({ event: event.name, ecommerce: event.params })
 
