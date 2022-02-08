9a183c1b6c6005a4c3eae69b64734a1b5150c4b3
Lucas Feijó <lucas.portela@vtex.com.br>
2022-02-08 15:10:35 -0300

    Add Shimmer component to handle shimmer effect

---
+++ src/components/skeletons/Shimmer/Shimmer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './shimmer.scss'
+
+const Shimmer = () => {
+  return (
+    <div data-store-shimmer>
+      <div data-shimmer />
+    </div>
+  )
+}
+
+export default Shimmer
