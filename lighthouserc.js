dc7f6e31b16536606c2315ff0718c38dc059f1a9
Tiago Gimenes <tlgimenes@gmail.com>
2022-02-14 18:18:11 -0300

    upgrade lighthouse config

--- lighthouserc.js
+++ lighthouserc.js
@@ -3,7 +3,7 @@ const VTEXLHConfig = require('@vtex/lighthouse-config').default
 const { lighthouse: lh } = require('./store.config')
 
 module.exports = VTEXLHConfig({
-  urls: Object.values(lh.pages),
+  urls: [Object.values(lh.pages)[0]],
   server: lh.server,
   assertions: {
     'csp-xss': 'off',
