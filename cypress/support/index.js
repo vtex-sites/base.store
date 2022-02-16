7e91b59c257eb44655e66ec24b8d5c89537d458a
Cezar <czrgmrs@gmail.com>
2022-02-16 00:42:51 -0300

    include auth header

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -1,3 +1,10 @@
 import './commands'
 import '@testing-library/cypress/add-commands'
 import 'cypress-axe'
+
+beforeEach(() => {
+  cy.intercept(
+    { middleware: true },
+    (req) => (req.headers.authorization = Cypress.env('AUTHORIZATION'))
+  )
+})
