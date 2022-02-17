822c7c48d3c82e309d0bc0e8a4e353a57f7b374b
Emerson Laurentino <emerson@laurentino.co>
2022-02-17 14:21:29 -0300

    feat: opensearch

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,6 +34,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-sass`,
+    `gatsby-plugin-react-helmet`,
     {
       resolve: 'gatsby-plugin-manifest',
       options: {
--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,10 +1,11 @@
+import type { PropsWithChildren } from 'react'
 import React, { lazy, Suspense } from 'react'
+import { Helmet } from 'react-helmet'
 import Alert from 'src/components/common/Alert'
 import Footer from 'src/components/common/Footer'
 import Navbar from 'src/components/common/Navbar'
 import Toast from 'src/components/common/Toast'
 import { useUI } from 'src/sdk/ui'
-import type { PropsWithChildren } from 'react'
 
 const CartSidebar = lazy(() => import('src/components/cart/CartSidebar'))
 
@@ -13,6 +14,15 @@ function Layout({ children }: PropsWithChildren<unknown>) {
 
   return (
     <div id="layout">
+      <Helmet>
+        <link
+          rel="search"
+          type="application/opensearchdescription+xml"
+          href="/opensearch.xml"
+          title="Base"
+        />
+      </Helmet>
+
       <Alert>
         Get 10% off today:&nbsp;<span>NEW10</span>
       </Alert>
