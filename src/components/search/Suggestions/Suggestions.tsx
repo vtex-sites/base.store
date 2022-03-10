f0809ce1c2c000cacaab23ac3051fe17bc28f392
Lucas Feijó <lucas.portela@vtex.com.br>
2022-03-10 11:18:10 -0300

    Code and styling improvements

--- src/components/search/Suggestions/Suggestions.tsx
+++ src/components/search/Suggestions/Suggestions.tsx
@@ -1,12 +1,13 @@
 import type { HTMLAttributes } from 'react'
 import React, { forwardRef } from 'react'
 import { List as UIList } from '@faststore/ui'
-import Link from 'src/components/ui/Link'
+import Button from 'src/components/ui/Button'
 
 import SuggestionProductCard from '../SuggestionProductCard'
 
 import './suggestions.scss'
 
+const MAX_SUGGESTIONS = 5
 const MAX_SUGGESTED_PRODUCTS = 4
 const SUGGESTED_PRODUCTS = [
   {
@@ -33,11 +34,7 @@ const SUGGESTED_PRODUCTS = [
   },
 ]
 
-const SEARCH_SUGGESTIONS = [
-  'Sony MX',
-  'Sony MV-100 Headphone',
-  'Sony M2000 Earbuds',
-]
+const SUGGESTIONS = ['Sony MX', 'Sony MV-100 Headphone', 'Sony M2000 Earbuds']
 
 export interface SuggestionsProps extends HTMLAttributes<HTMLDivElement> {
   /**
@@ -55,52 +52,80 @@ const Suggestions = forwardRef<HTMLDivElement, SuggestionsProps>(
     { testId = 'suggestions', term = '', ...otherProps },
     ref
   ) {
-    const boldTerm = (suggestion: string, _term: string) => {
-      const suggestionChars = suggestion.split(_term)
+    const suggestions =
+      SUGGESTED_PRODUCTS.length > 0
+        ? SUGGESTIONS.slice(0, MAX_SUGGESTIONS)
+        : SUGGESTIONS
+
+    const handleSuggestions = (suggestion: string, searchTerm: string) => {
+      const suggestionSubstring = suggestion
+        .toLowerCase()
+        .split(searchTerm.toLowerCase())
 
       return (
         <p>
-          {suggestionChars.map((char, index) => (
-            <>
-              {char}
-              {index !== suggestionChars.length - 1 && (
-                <b className="suggestions__bold-term">{_term}</b>
-              )}
-            </>
-          ))}
+          {suggestionSubstring.map((substring, indexSubstring) => {
+            const formatSearchTerm = () => {
+              if (indexSubstring === 0) {
+                return searchTerm
+                  .split('')
+                  .map((char, idx) =>
+                    idx === 0 && suggestion.indexOf(char.toUpperCase()) === 0
+                      ? char.toUpperCase()
+                      : char.toLowerCase()
+                  )
+              }
+
+              return searchTerm.toLowerCase()
+            }
+
+            return (
+              <>
+                <b className="suggestions__suggestion-bold">
+                  {indexSubstring === 0
+                    ? substring.charAt(0).toUpperCase() + substring.slice(1)
+                    : substring}
+                </b>
+                {indexSubstring !== suggestionSubstring.length - 1 &&
+                  formatSearchTerm()}
+              </>
+            )
+          })}
         </p>
       )
     }
 
     return (
-      <div
+      <section
         ref={ref}
         data-testid={testId}
         data-store-suggestions
         className="suggestions"
         {...otherProps}
       >
-        <UIList>
-          {SEARCH_SUGGESTIONS.map((suggestion, index) => (
-            <Link key={index} to="/">
-              {boldTerm(suggestion, term)}
-            </Link>
+        <UIList data-suggestions-list className="suggestions__section">
+          {suggestions.map((suggestion, index) => (
+            <Button
+              key={index}
+              onClick={() => null}
+              className="suggestions__suggestion"
+            >
+              {handleSuggestions(suggestion, term)}
+            </Button>
           ))}
         </UIList>
 
-        <div data-suggestions-suggested-products>
-          <p className="suggestions__suggested-products-title">
-            Suggested Products
-          </p>
+        <div className="suggestions__section">
+          <p className="suggestions__title">Suggested Products</p>
           <UIList>
-            {SUGGESTED_PRODUCTS.slice(0, MAX_SUGGESTED_PRODUCTS + 1).map(
+            {SUGGESTED_PRODUCTS.slice(0, MAX_SUGGESTED_PRODUCTS).map(
               (product, index) => (
                 <SuggestionProductCard key={index} product={product} />
               )
             )}
           </UIList>
         </div>
-      </div>
+      </section>
     )
   }
 )
