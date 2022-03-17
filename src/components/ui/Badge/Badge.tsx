855ffc0dba9cd820577e573613093623ab9649ff
Lucas Feijó <lucas.portela@vtex.com.br>
2022-03-17 15:18:58 -0300

    Adds new `Badge` interactive variation

--- src/components/ui/Badge/Badge.tsx
+++ src/components/ui/Badge/Badge.tsx
@@ -1,18 +1,27 @@
 import { Badge as UIBadge } from '@faststore/ui'
 import type { ReactNode } from 'react'
 import React from 'react'
+import IconButton from 'src/components/ui/IconButton'
+import Icon from 'src/components/ui/Icon'
 
 import './badge.scss'
 
-export type BadgeVariants = 'info' | 'highlighted' | 'neutral'
+export type BadgeVariants = 'info' | 'highlighted' | 'neutral' | 'interactive'
 
 type Props = {
   small?: boolean
   variant?: BadgeVariants
   children: ReactNode
+  onClose?: () => void
 }
 
-const Badge = ({ small = false, variant, children, ...otherProps }: Props) => {
+const Badge = ({
+  small = false,
+  variant,
+  children,
+  onClose,
+  ...otherProps
+}: Props) => {
   return (
     <UIBadge
       className="badge"
@@ -20,7 +29,21 @@ const Badge = ({ small = false, variant, children, ...otherProps }: Props) => {
       data-store-badge-variant={variant}
       {...otherProps}
     >
-      {children}
+      <span>{children}</span>
+      {variant === 'interactive' && (
+        <IconButton
+          onClick={onClose}
+          aria-label="Remove badge"
+          icon={
+            <Icon
+              name="X"
+              weight="bold"
+              width={small ? 12 : 16}
+              height={small ? 12 : 16}
+            />
+          }
+        />
+      )}
     </UIBadge>
   )
 }
