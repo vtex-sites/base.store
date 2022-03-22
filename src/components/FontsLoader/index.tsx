2dd92504cc44d761a90ddf016dace78134e53ddf
igorbrasileiro <brasileiro456@gmail.com>
2022-03-22 14:08:42 -0300

    Swap from promises to setTimeout

--- src/components/FontsLoader/index.tsx
+++ src/components/FontsLoader/index.tsx
@@ -5,7 +5,7 @@ export function LoadFonts() {
   return (
     <script
       dangerouslySetInnerHTML={{
-        __html: `"fonts"in document?Promise.all([document.fonts.load("400 1em Lato"),document.fonts.load("700 1em Lato"),document.fonts.load("900 1em Lato")]).then(()=>{document.body.classList.add("body-font")}):document.body.classList.add("body-font");
+        __html: `"fonts"in document?setTimeout(()=>{document.fonts.load("400 1em Lato"),document.fonts.load("700 1em Lato"),document.fonts.load("900 1em Lato"),document.body.classList.add("body-font")},0):document.body.classList.add("body-font");
 `,
       }}
     />
