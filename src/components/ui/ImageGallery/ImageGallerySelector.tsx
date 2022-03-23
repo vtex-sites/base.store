a8f7868fc9b3ede3646e8537d6c3dd3a8fab0ea1
Daniel Zanzini <daniel.zanzini@vtex.com.br>
2022-03-23 14:46:39 -0300

    Uses scroll instead of slider on gallery

--- src/components/ui/ImageGallery/ImageGallerySelector.tsx
+++ src/components/ui/ImageGallery/ImageGallerySelector.tsx
@@ -10,21 +10,6 @@ interface Props extends HTMLAttributes<HTMLDivElement> {
   itemsPerPage: number
 }
 
-function getTransformValue(
-  totalItems: number,
-  currentPage: number,
-  itemsPerPage: number
-) {
-  // We use 100 to represent the full width of the parent element
-  const slideWidth = 100 / totalItems
-
-  // slideWidth gives us the full width value for sliding through all elements.
-  // We then multiply the slideWidth by (currentPage * itemsPerPage) to calculate the
-  // transformValue for each page (the more we slide forward, the more we have to slide in total).
-  // It is negative so the transition slides the items to the left.
-  return -(slideWidth * currentPage * itemsPerPage)
-}
-
 function ImageGallerySelector({
   itemsPerPage,
   children,
@@ -32,21 +17,9 @@ function ImageGallerySelector({
   const elements = Children.toArray(children)
   const elementCount = elements.length
 
-  const { handlers, slide, sliderState, sliderDispatch } = useSlider({
-    totalItems: elementCount,
-    itemsPerPage,
-    infiniteMode: false,
-  })
-
   if (!elementCount) {
     return null
   }
-
-  const transformValue = getTransformValue(
-    elementCount,
-    sliderState.currentPage,
-    itemsPerPage
-  )
 
   return (
     <section
@@ -54,67 +27,42 @@ function ImageGallerySelector({
       aria-roledescription="carousel"
       aria-label="Product images"
     >
-      {elementCount > itemsPerPage && sliderState.currentPage !== 0 && (
+      {elementCount > itemsPerPage && (
         <IconButton
           aria-label="backward slide image selector"
           icon={<BackwardArrowIcon color="#323845" />}
           onClick={() => {
-            if (sliderState.sliding) {
-              return
-            }
+            const container = document.getElementById('myScrollSlider')
 
-            slide('previous', sliderDispatch)
+            if (container) {
+              if (container.scrollHeight > container.clientHeight) {
+                container.scrollTop -= 200
+              } else {
+                container.scrollLeft -= 200
+              }
+            }
           }}
         />
       )}
-      <div data-carousel-track-container {...handlers}>
-        <div
-          data-carousel-track
-          style={{
-            width: `${(elementCount * 100) / itemsPerPage}%`,
-            transform: `translate3d(${transformValue}%, 0, 0)`,
-          }}
-          onTransitionEnd={() => {
-            sliderDispatch({
-              type: 'STOP_SLIDE',
-            })
-
-            if (sliderState.currentItem >= elementCount) {
-              sliderDispatch({
-                type: 'GO_TO_PAGE',
-                payload: {
-                  pageIndex: 0,
-                  shouldSlide: false,
-                },
-              })
-            }
-
-            if (sliderState.currentItem < 0) {
-              sliderDispatch({
-                type: 'GO_TO_PAGE',
-                payload: {
-                  pageIndex: sliderState.totalPages - 1,
-                  shouldSlide: false,
-                },
-              })
-            }
-          }}
-        >
-          {elements.map((el, idx) => {
-            return <div key={idx}>{el}</div>
-          })}
-        </div>
+      <div id="myScrollSlider" data-carousel-track>
+        {elements.map((el, idx) => {
+          return <div key={idx}>{el}</div>
+        })}
       </div>
       {elementCount > itemsPerPage && (
         <IconButton
           aria-label="forward slide image selector"
           icon={<ForwardArrowIcon color="#323845" />}
           onClick={() => {
-            if (sliderState.sliding) {
-              return
-            }
+            const container = document.getElementById('myScrollSlider')
 
-            slide('next', sliderDispatch)
+            if (container) {
+              if (container?.scrollHeight > container?.clientHeight) {
+                container.scrollTop += 200
+              } else {
+                container.scrollLeft += 200
+              }
+            }
           }}
         />
       )}
