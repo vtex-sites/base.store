0329f895a1ce64561fa1b1204bcac61f94b17a59
Sara Lins <saranicoly0@gmail.com>
2022-03-25 18:27:09 -0300

    align items

--- src/components/common/PostalCode/PostalCodeBar.tsx
+++ src/components/common/PostalCode/PostalCodeBar.tsx
@@ -6,18 +6,20 @@ import './postal-code.scss'
 
 export default function PostalCodeInput() {
   return (
-    <Alert
-      data-store-alert
-      icon={<Icon name="MapPin" width={24} height={24} />}
-    >
-      Set your location
-      <IconButton
-        icon={<Icon name="CaretRight" width={24} height={24} />}
-        aria-label="Set location"
-        onClick={() => {
-          window.scrollTo(0, 1000)
-        }}
-      />
-    </Alert>
+    <div className="postal-code-bar">
+      <Alert
+        data-store-alert
+        icon={<Icon name="MapPin" width={24} height={24} />}
+      >
+        <span>Set your location</span>
+        <IconButton
+          icon={<Icon name="CaretRight" width={24} height={24} />}
+          aria-label="Set location"
+          onClick={() => {
+            window.scrollTo(0, 1000)
+          }}
+        />
+      </Alert>
+    </div>
   )
 }
