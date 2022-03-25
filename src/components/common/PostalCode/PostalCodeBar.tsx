fe56aba826bb21b38182cec377feea5355123c02
Sara Lins <saranicoly0@gmail.com>
2022-03-25 17:46:05 -0300

    create basic component

--- src/components/common/PostalCode/PostalCodeBar.tsx
+++ src/components/common/PostalCode/PostalCodeBar.tsx
@@ -1,5 +1,23 @@
+import { IconButton } from '@faststore/ui'
 import React from 'react'
+import Alert from 'src/components/ui/Alert'
+import Icon from 'src/components/ui/Icon'
+import './postal-code.scss'
 
 export default function PostalCodeInput() {
-  return <span>Postal code bar</span>
+  return (
+    <Alert
+      data-store-alert
+      icon={<Icon name="MapPin" width={24} height={24} />}
+    >
+      Set your location
+      <IconButton
+        icon={<Icon name="CaretRight" width={24} height={24} />}
+        aria-label="Set location"
+        onClick={() => {
+          window.scrollTo(0, 1000)
+        }}
+      />
+    </Alert>
+  )
 }
