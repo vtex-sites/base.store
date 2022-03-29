05cdb16bd74768ddfe660482f2e2924b98f28cfc
Fanny Chien <fannychienn@gmail.com>
2022-03-29 12:38:59 -0300

    Add error variation, message text and button

--- src/components/ui/InputText/InputText.tsx
+++ src/components/ui/InputText/InputText.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Input as UIInput, Label as UILabel } from '@faststore/ui'
 import type { InputProps } from '@faststore/ui'
+import Button from 'src/components/ui/Button'
 
 import './input-text.scss'
 
@@ -8,6 +9,8 @@ export type InputTextProps = {
   id: string
   label: string
   placeholder?: string
+  errorMessage?: string
+  buttonLabel?: string
 }
 
 type Props = InputTextProps & InputProps
@@ -16,14 +19,25 @@ const InputText = ({
   id,
   label,
   type = 'text',
-  variant,
+  errorMessage,
+  buttonLabel = 'Apply',
   placeholder = ' ', // needed to style float label using `placeholder-shown`
   ...otherProps
 }: Props) => {
   return (
-    <div data-fs-input-text data-fs-input-text-variant={variant}>
+    <div
+      data-fs-input-text
+      data-fs-input-text-error={!!errorMessage}
+      data-fs-input-text-button={!!buttonLabel}
+    >
       <UIInput type={type} id={id} placeholder={placeholder} {...otherProps} />
       <UILabel htmlFor={id}>{label}</UILabel>
+      {buttonLabel && (
+        <Button variant="tertiary" data-fs-input-text-button>
+          {buttonLabel}
+        </Button>
+      )}
+      {errorMessage && <span data-fs-input-text-message>{errorMessage}</span>}
     </div>
   )
 }
