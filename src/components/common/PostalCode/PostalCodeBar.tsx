50bf28bf9a9eea11fb6cad7382c6fee5bcd7ce96
Sara Lins <saranicoly0@gmail.com>
2022-03-30 10:18:58 -0300

    remove Alert and use button instead

--- src/components/common/PostalCode/PostalCodeBar.tsx
+++ src/components/common/PostalCode/PostalCodeBar.tsx
@@ -1,6 +1,5 @@
-import { IconButton } from '@faststore/ui'
 import React from 'react'
-import Alert from 'src/components/ui/Alert'
+import Button from 'src/components/ui/Button'
 import Icon from 'src/components/ui/Icon'
 import './postal-code.scss'
 
@@ -11,34 +10,26 @@ interface PostalCodeBarProps {
 export default function PostalCodeInput({ content }: PostalCodeBarProps) {
   return (
     <div className="postal-code-bar">
-      <Alert
+      <Button
         data-store-alert
-        icon={<Icon name="MapPin" width={24} height={24} />}
+        onClick={() => {
+          window.scrollTo(0, 1000)
+        }}
       >
         {content ? (
           <>
+            <Icon name="MapPin" width={24} height={24} />
             {content}
-            <IconButton
-              icon={<Icon name="CaretRight" width={24} height={24} />}
-              aria-label="Set location"
-              onClick={() => {
-                window.scrollTo(0, 1000)
-              }}
-            />
+            <Icon name="CaretRight" width={24} height={24} />
           </>
         ) : (
           <>
+            <Icon name="MapPin" width={24} height={24} />
             <span>Set your location</span>
-            <IconButton
-              icon={<Icon name="CaretRight" width={24} height={24} />}
-              aria-label="Set location"
-              onClick={() => {
-                window.scrollTo(0, 1000)
-              }}
-            />
+            <Icon name="CaretRight" width={24} height={24} />
           </>
         )}
-      </Alert>
+      </Button>
     </div>
   )
 }
