7fc7445aae8d71368c079bdd56dcb06dbb4612e6
Emerson Laurentino <emersonlaurentino@hotmail.com>
2022-03-31 17:06:50 -0300

    Update src/sdk/ui/useOnClickOutside.ts
    Co-authored-by: Igor Brasileiro <brasileiro456@gmail.com>

--- src/sdk/ui/useOnClickOutside.ts
+++ src/sdk/ui/useOnClickOutside.ts
@@ -26,12 +26,14 @@ export default function useOnClickOutside<T extends HTMLElement = HTMLElement>(
         document.removeEventListener('touchstart', listener)
       }
     },
-    // Add ref and handler to effect dependencies
-    // It's worth noting that because passed in handler is a new ...
-    // ... function on every render that will cause this effect ...
-    // ... callback/cleanup to run every render. It's not a big deal ...
-    // ... but to optimize you can wrap handler in useCallback before ...
-    // ... passing it into this hook.
+    /**
+    * Add ref and handler to effect dependencies.
+    * It's worth noting that because passed in handler is a new
+    * function on every render that will cause this effect 
+    * callback/cleanup to run every render. It's not a big deal 
+    * but to optimize you can wrap handler in useCallback before 
+    * passing it into this hook.
+    */
     [ref, handler]
   )
 }
