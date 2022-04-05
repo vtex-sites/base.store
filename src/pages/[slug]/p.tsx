ec9de4b23bec43ec3c2ddf747c449b976c5c5e2d
Tiago Gimenes <tlgimenes@gmail.com>
2022-04-05 17:33:19 -0300

    add stale-while-revalidate cache header

--- src/pages/[slug]/p.tsx
+++ src/pages/[slug]/p.tsx
@@ -210,7 +210,7 @@ export const getServerData = async ({
       status: 200,
       props: data ?? {},
       headers: {
-        'cache-control': 'public, max-age=0, must-revalidate',
+        'cache-control': 'public, max-age=0, stale-while-revalidate=31536000',
       },
     }
   } catch (err) {
