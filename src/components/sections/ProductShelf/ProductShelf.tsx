08b6f80b6f1804957f225d349a435168ef7071bf
eduardoformiga <eduardo.formiga@vtex.com.br>
2022-04-06 14:23:49 -0300

    Scaffolds shelf theme structure

--- src/components/sections/ProductShelf/ProductShelf.tsx
+++ src/components/sections/ProductShelf/ProductShelf.tsx
@@ -27,9 +27,9 @@ function ProductShelf({
       className={`layout__section ${withDivisor ? 'shelf__divisor' : ''}`}
     >
       <h2 className="text__title-section layout__content">{title}</h2>
-      <div data-product-shelf-content>
+      <div data-fs-product-shelf>
         <ProductShelfSkeleton loading={products === undefined}>
-          <ul data-product-shelf-items className="layout__content">
+          <ul data-fs-product-shelf-items className="layout__content">
             {products?.edges.map((product, idx) => (
               <li key={`${product.node.id}`}>
                 <ProductCard product={product.node} index={idx + 1} />
