18393281876660876ef95222a8344a4a889bfefa
Israel Costa <israelswf@gmail.com>
2022-04-08 11:39:02 -0300

    Adjust Incentives to render CMS data

---
+++ src/components/sections/Incentives/incentivesMock.js
@@ -0,0 +1,29 @@
+const incentives = [
+  {
+    icon: 'ShieldCheck',
+    firstLineText: 'Trusted',
+    secondLineText: 'by SafeCon',
+  },
+  {
+    icon: 'Medal',
+    firstLineText: 'Quality',
+    secondLineText: 'Products',
+  },
+  {
+    icon: 'CircleWavyCheck',
+    firstLineText: '3-years',
+    secondLineText: 'Guarantee',
+  },
+  {
+    icon: 'Storefront',
+    firstLineText: 'Pickup',
+    secondLineText: 'Options',
+  },
+  {
+    icon: 'Truck',
+    firstLineText: 'Free',
+    secondLineText: 'Shipping',
+  },
+]
+
+export default incentives
