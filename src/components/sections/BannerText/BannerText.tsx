ebe78c98fd09b310e0d1fe740c778a28039db2d7
Lucas Feijó <lucas.portela@vtex.com.br>
2022-04-08 13:17:37 -0300

    Updates `BannerText` to use new tokens

--- src/components/sections/BannerText/BannerText.tsx
+++ src/components/sections/BannerText/BannerText.tsx
@@ -1,46 +1,59 @@
 import { Banner, BannerContent, BannerLink } from '@faststore/ui'
 import { ButtonLink } from 'src/components/ui/Button'
-import type { InputHTMLAttributes } from 'react'
+import type { HTMLAttributes } from 'react'
 
 import Section from '../Section'
 
-export interface BannerTextProps extends InputHTMLAttributes<HTMLDivElement> {
-  /**
-   * The content for the h2 tag.
-   */
-  title: string
+type BannerTextVariant =
+  | {
+      variant?: 'primary'
+      /**
+       * The content for the h2 tag.
+       */
+      title: string
+      caption?: never
+    }
+  | {
+      variant: 'secondary'
+      /**
+       * The content for the h2 tag.
+       */
+      title: string
+      /**
+       * The content for the p tag below the h2.
+       */
+      caption: string
+    }
 
-  /**
-   * The content for the p tag below the h2.
-   */
-  caption: string
-
+interface BaseProps extends HTMLAttributes<HTMLDivElement> {
   /**
    * The href used at the link
    */
   actionPath: string
-
   /**
    * The label used at the link
    */
   actionLabel: string
 }
 
+export type BannerTextProps = BaseProps & BannerTextVariant
+
 function BannerText({
   title,
   caption,
   actionPath,
   actionLabel,
+  variant = 'primary',
 }: BannerTextProps) {
   return (
     <Section className="layout__section">
-      <Banner>
-        <BannerContent className="layout__content">
-          <div data-banner-text>
+      <Banner data-fs-banner-text data-fs-banner-text-variant={variant}>
+        <BannerContent data-fs-banner-text-content className="layout__content">
+          <div>
             <h2>{title}</h2>
-            <p>{caption}</p>
+            {variant === 'secondary' && caption && <p>{caption}</p>}
           </div>
-          <BannerLink>
+          <BannerLink data-fs-banner-text-link>
             <ButtonLink to={actionPath} inverse>
               {actionLabel}
             </ButtonLink>
