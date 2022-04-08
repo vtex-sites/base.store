40e619e2f23d13844c4a3441133ba5b02f76e92a
Arthur Andrade <arthurfelandrade@gmail.com>
2022-04-08 15:11:31 -0300

    feat: refactor storybook config
    Signed-off-by: Arthur Andrade <arthurfelandrade@gmail.com>

--- .storybook/main.js
+++ .storybook/main.js
@@ -7,6 +7,7 @@ module.exports = {
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
+    'storybook-addon-gatsby',
   ],
   framework: '@storybook/react',
   core: {
@@ -14,13 +15,6 @@ module.exports = {
   },
   staticDirs: ['../static'],
   webpackFinal: async (config) => {
-    // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
-    config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
-    // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
-    config.module.rules[0].use[0].options.plugins.push(
-      require.resolve('babel-plugin-remove-graphql-queries')
-    )
-
     config.module.rules[0].use[0].options.plugins.push(
       require.resolve('@vtex/graphql-utils/babel')
     )
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,24 +5,7 @@ import '!style-loader!css-loader!sass-loader!../src/styles/global/typography.scs
 import '!style-loader!css-loader!sass-loader!../src/styles/global/layout.scss'
 import '!style-loader!css-loader!sass-loader!../src/styles/global/components.scss'
 
-import { action } from '@storybook/addon-actions'
 import SBTheme from './theme'
-
-// Gatsby's Link overrides:
-// Gatsby Link calls the `enqueue` & `hovering` methods on the global variable ___loader.
-// This global object isn't set in storybook context, requiring you to override it to empty functions (no-op),
-// so Gatsby Link doesn't throw errors.
-global.___loader = {
-  enqueue: () => {},
-  hovering: () => {},
-}
-// This global variable prevents the "__BASE_PATH__ is not defined" error inside Storybook.
-global.__BASE_PATH__ = '/'
-// Navigating through a gatsby app using gatsby-link or any other gatsby component will use the `___navigate` method.
-// In Storybook, it makes more sense to log an action than doing an actual navigate. Check out the actions addon docs for more info: https://storybook.js.org/docs/react/essentials/actions
-window.___navigate = (pathname) => {
-  action('NavigateTo:')(pathname)
-}
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
