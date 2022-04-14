bcd3b01eca87a56810a3d774d8df6cd6b1d19dee
Renata Motta <renatamotta.m@gmail.com>
2022-04-14 14:33:18 -0300

    Feat: Hero Theming (#435)
    * Updates Hero to use new tokens
    
    * Decreases typography's mobile scale
    
    * Simplifies text tokens
    
    * Simplifies text tokens a little more
    
    * Improves Default Hero on desktop view
    
    * Improves tokens to match with Primary and Secondary variations
    
    * Updates secondary title size to use title-page token
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    
    * Updates Changelog
    
    * Triggers building
    
    * Updates Changelog
    
    * Attempts to fix mobile text scale
    
    * Updates line-height tokens
    
    * Removes Stylelint work around fixed on #430
    
    * Tweaks CSS files's sections titles
    
    * Trigger CI with an empty commit
    
    * Creates color variantes for Hero
    
    * Adds Hero to Storybook
    
    * Updates argTypes and separates Primary and Secondary types
    
    * Tweaks section width
    
    Co-authored-by: Fanny Chien <fannychienn@gmail.com>
    Co-authored-by: Eduardo Formiga <eduardo.formiga@gmail.com>
    Co-authored-by: Filipe W. Lima <filipe.lima@vtex.com.br>
    Co-authored-by: Filipe W. Lima <fwl.ufpe@gmail.com>

--- src/components/sections/Hero/Hero.tsx
+++ src/components/sections/Hero/Hero.tsx
@@ -9,12 +9,14 @@ import Image from 'src/components/ui/Image/Image'
 
 import Section from '../Section'
 
-type Variant = 'default' | 'small'
+type Variant = 'primary' | 'secondary'
+type ColorVariant = 'main' | 'light' | 'accent'
 
-interface HeroProps {
+export interface HeroProps {
   title: string
   subtitle: string
   variant?: Variant
+  colorVariant?: ColorVariant
   linkText?: string
   link?: string
   icon?: JSX.Element
@@ -25,7 +27,8 @@ interface HeroProps {
 const Hero = ({
   title,
   subtitle,
-  variant = 'default',
+  variant = 'primary',
+  colorVariant = 'main',
   linkText,
   link,
   icon,
@@ -34,8 +37,12 @@ const Hero = ({
 }: HeroProps) => {
   return (
     <Section>
-      <UIHero data-hero-variant={variant}>
-        <HeroImage>
+      <UIHero
+        data-fs-hero
+        data-fs-hero-variant={variant}
+        data-fs-hero-color-variant={colorVariant}
+      >
+        <HeroImage data-fs-hero-image>
           <Image
             preload
             loading="eager"
@@ -46,21 +53,23 @@ const Hero = ({
             sizes="(max-width: 768px) 70vw, 50vw"
           />
         </HeroImage>
-        <HeroContent aria-labelledby="hero-heading">
-          <div data-hero-wrapper className="layout__content">
-            <div data-hero-info>
+        <HeroContent data-fs-hero-content aria-labelledby="hero-heading">
+          <div data-fs-hero-wrapper className="layout__content">
+            <div data-fs-hero-info>
               <h1 id="hero-heading">{title}</h1>
 
-              <p data-hero-text-body>{subtitle}</p>
+              <p data-fs-hero-text-body>{subtitle}</p>
               {!!link && (
                 <HeroLink>
-                  <ButtonLink to={link} inverse>
+                  <ButtonLink to={link} inverse={colorVariant === 'main'}>
                     {linkText} <Icon name="ArrowRight" width={24} height={24} />
                   </ButtonLink>
                 </HeroLink>
               )}
             </div>
-            {!!icon && <div data-hero-icon>{icon}</div>}
+            {variant === 'secondary' && icon && (
+              <div data-fs-hero-icon>{icon}</div>
+            )}
           </div>
         </HeroContent>
       </UIHero>
